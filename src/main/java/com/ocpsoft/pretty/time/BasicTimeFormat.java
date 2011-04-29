164cb4a600b39c34732ba89cec52fbbd8b3c7508
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2011-04-29 12:58:21 -0400

    Integrated resource bundles and pull requests

--- src/main/java/com/ocpsoft/pretty/time/BasicTimeFormat.java
+++ src/main/java/com/ocpsoft/pretty/time/BasicTimeFormat.java
@@ -27,163 +27,162 @@ package com.ocpsoft.pretty.time;
  */
 public class BasicTimeFormat implements TimeFormat
 {
-    private static final String NEGATIVE = "-";
-    public static final String SIGN = "%s";
-    public static final String QUANTITY = "%n";
-    public static final String UNIT = "%u";
+   private static final String NEGATIVE = "-";
+   public static final String SIGN = "%s";
+   public static final String QUANTITY = "%n";
+   public static final String UNIT = "%u";
 
-    private String pattern = "";
-    private String futurePrefix = "";
-    private String futureSuffix = "";
-    private String pastPrefix = "";
-    private String pastSuffix = "";
-    private int roundingTolerance = 50;
+   private String pattern = "";
+   private String futurePrefix = "";
+   private String futureSuffix = "";
+   private String pastPrefix = "";
+   private String pastSuffix = "";
+   private int roundingTolerance = 50;
 
-    public String format(final Duration duration)
-    {
-        String sign = getSign(duration);
-        String unit = getGramaticallyCorrectName(duration);
-        long quantity = getQuantity(duration);
+   public String format(final Duration duration)
+   {
+      String sign = getSign(duration);
+      String unit = getGramaticallyCorrectName(duration);
+      long quantity = getQuantity(duration);
 
-        String result = applyPattern(sign, unit, quantity);
-        result = decorate(sign, result);
+      String result = applyPattern(sign, unit, quantity);
+      result = decorate(sign, result);
 
-        return result;
-    }
+      return result;
+   }
 
-    private String decorate(final String sign, String result)
-    {
-        if (NEGATIVE.equals(sign))
-        {
-            result = pastPrefix + " " + result + " " + pastSuffix;
-        }
-        else
-        {
-            result = futurePrefix + " " + result + " " + futureSuffix;
-        }
-        return result.trim();
-    }
+   private String decorate(final String sign, String result)
+   {
+      if (NEGATIVE.equals(sign))
+      {
+         result = pastPrefix + " " + result + " " + pastSuffix;
+      }
+      else
+      {
+         result = futurePrefix + " " + result + " " + futureSuffix;
+      }
+      return result.replaceAll("\\s+", " ").trim();
+   }
 
-    private String applyPattern(final String sign, final String unit, final long quantity)
-    {
-        String result = pattern.replaceAll(SIGN, sign);
-        result = result.replaceAll(QUANTITY, String.valueOf(quantity));
-        result = result.replaceAll(UNIT, unit);
-        return result;
-    }
+   private String applyPattern(final String sign, final String unit, final long quantity)
+   {
+      String result = pattern.replaceAll(SIGN, sign);
+      result = result.replaceAll(QUANTITY, String.valueOf(quantity));
+      result = result.replaceAll(UNIT, unit);
+      return result;
+   }
 
-    private long getQuantity(final Duration duration)
-    {
-        long quantity = Math.abs(duration.getQuantity());
+   private long getQuantity(final Duration duration)
+   {
+      long quantity = Math.abs(duration.getQuantity());
 
-        if (duration.getDelta() != 0)
-        {
-            double threshold = Math
+      if (duration.getDelta() != 0)
+      {
+         double threshold = Math
                     .abs(((double) duration.getDelta() / (double) duration.getUnit().getMillisPerUnit()) * 100);
-            if (threshold > roundingTolerance)
-            {
-                quantity = quantity + 1;
-            }
-        }
-        return quantity;
-    }
+         if (threshold > roundingTolerance)
+         {
+            quantity = quantity + 1;
+         }
+      }
+      return quantity;
+   }
 
-    private String getGramaticallyCorrectName(final Duration d)
-    {
-        String result = d.getUnit().getName();
-        if ((Math.abs(getQuantity(d)) == 0) || (Math.abs(getQuantity(d)) > 1))
-        {
-            result = d.getUnit().getPluralName();
-        }
-        return result;
-    }
+   private String getGramaticallyCorrectName(final Duration d)
+   {
+      String result = d.getUnit().getName();
+      if ((Math.abs(getQuantity(d)) == 0) || (Math.abs(getQuantity(d)) > 1))
+      {
+         result = d.getUnit().getPluralName();
+      }
+      return result;
+   }
 
-    private String getSign(final Duration d)
-    {
-        if (d.getQuantity() < 0)
-        {
-            return NEGATIVE;
-        }
-        return "";
-    }
+   private String getSign(final Duration d)
+   {
+      if (d.getQuantity() < 0)
+      {
+         return NEGATIVE;
+      }
+      return "";
+   }
 
-    /*
-     * Builder Setters
-     */
-    public BasicTimeFormat setPattern(final String pattern)
-    {
-        this.pattern = pattern;
-        return this;
-    }
+   /*
+    * Builder Setters
+    */
+   public BasicTimeFormat setPattern(final String pattern)
+   {
+      this.pattern = pattern;
+      return this;
+   }
 
-    public BasicTimeFormat setFuturePrefix(final String futurePrefix)
-    {
-        this.futurePrefix = futurePrefix.trim();
-        return this;
-    }
+   public BasicTimeFormat setFuturePrefix(final String futurePrefix)
+   {
+      this.futurePrefix = futurePrefix.trim();
+      return this;
+   }
 
-    public BasicTimeFormat setFutureSuffix(final String futureSuffix)
-    {
-        this.futureSuffix = futureSuffix.trim();
-        return this;
-    }
+   public BasicTimeFormat setFutureSuffix(final String futureSuffix)
+   {
+      this.futureSuffix = futureSuffix.trim();
+      return this;
+   }
 
-    public BasicTimeFormat setPastPrefix(final String pastPrefix)
-    {
-        this.pastPrefix = pastPrefix.trim();
-        return this;
-    }
+   public BasicTimeFormat setPastPrefix(final String pastPrefix)
+   {
+      this.pastPrefix = pastPrefix.trim();
+      return this;
+   }
 
-    public BasicTimeFormat setPastSuffix(final String pastSuffix)
-    {
-        this.pastSuffix = pastSuffix.trim();
-        return this;
-    }
+   public BasicTimeFormat setPastSuffix(final String pastSuffix)
+   {
+      this.pastSuffix = pastSuffix.trim();
+      return this;
+   }
 
-    /**
-     * The percentage of the current {@link TimeUnit}.getMillisPerUnit() for
-     * which the quantity may be rounded up by one.
-     * 
-     * @param roundingTolerance
-     * @return
-     */
-    public BasicTimeFormat setRoundingTolerance(final int roundingTolerance)
-    {
-        this.roundingTolerance = roundingTolerance;
-        return this;
-    }
+   /**
+    * The percentage of the current {@link TimeUnit}.getMillisPerUnit() for which the quantity may be rounded up by one.
+    * 
+    * @param roundingTolerance
+    * @return
+    */
+   public BasicTimeFormat setRoundingTolerance(final int roundingTolerance)
+   {
+      this.roundingTolerance = roundingTolerance;
+      return this;
+   }
 
-    /*
-     * Normal getters
-     */
+   /*
+    * Normal getters
+    */
 
-    public String getPattern()
-    {
-        return pattern;
-    }
+   public String getPattern()
+   {
+      return pattern;
+   }
 
-    public String getFuturePrefix()
-    {
-        return futurePrefix;
-    }
+   public String getFuturePrefix()
+   {
+      return futurePrefix;
+   }
 
-    public String getFutureSuffix()
-    {
-        return futureSuffix;
-    }
+   public String getFutureSuffix()
+   {
+      return futureSuffix;
+   }
 
-    public String getPastPrefix()
-    {
-        return pastPrefix;
-    }
+   public String getPastPrefix()
+   {
+      return pastPrefix;
+   }
 
-    public String getPastSuffix()
-    {
-        return pastSuffix;
-    }
+   public String getPastSuffix()
+   {
+      return pastSuffix;
+   }
 
-    public int getRoundingTolerance()
-    {
-        return roundingTolerance;
-    }
+   public int getRoundingTolerance()
+   {
+      return roundingTolerance;
+   }
 }

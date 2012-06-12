72ad5dbc8042095fc2ceaa0e495fc250340fc222
Muhammad Sabry <abosabry_2005@hotmail.com>
2012-06-12 14:21:14 +0200

    Added an Arabic resource bundle Resources_ar.java, and a corresponding test file test/java/org/ocpsoft/prettytime/PrettyTimeI18n_AR_Test.java.
    Signed-off-by: Muhammad Sabry Ali <Muhammad_Sabry_Ali@hotmail.com>

---
+++ src/main/java/org/ocpsoft/prettytime/i18n/Resources_ar.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+public class Resources_ar extends ListResourceBundle
+{
+    private static final Object[][] OBJECTS = new Object[][] {
+                                                              { "CenturyPattern", "%n %u" },
+                                                              { "CenturyFuturePrefix", "" },
+                                                              { "CenturyFutureSuffix", " من الآن" },
+                                                              { "CenturyPastPrefix", "" },
+                                                              { "CenturyPastSuffix", " مضت" },
+                                                              { "CenturyName", "قرن" },
+                                                              { "CenturyPluralName", "قرون" },
+                                                              { "DayPattern", "%n %u" },
+                                                              { "DayFuturePrefix", "" },
+                                                              { "DayFutureSuffix", " من الآن" },
+                                                              { "DayPastPrefix", "" },
+                                                              { "DayPastSuffix", " مضت" },
+                                                              { "DayName", "يوم" },
+                                                              { "DayPluralName", "ايام" },
+                                                              { "DecadePattern", "%n %u" },
+                                                              { "DecadeFuturePrefix", "" },
+                                                              { "DecadeFutureSuffix", " من الآن" },
+                                                              { "DecadePastPrefix", "" },
+                                                              { "DecadePastSuffix", " مضت" },
+                                                              { "DecadeName", "عقد" },
+                                                              { "DecadePluralName", "عقود" },
+                                                              { "HourPattern", "%n %u" },
+                                                              { "HourFuturePrefix", "" },
+                                                              { "HourFutureSuffix", " من الآن" },
+                                                              { "HourPastPrefix", "" },
+                                                              { "HourPastSuffix", " مضت" },
+                                                              { "HourName", "ساعة" },
+                                                              { "HourPluralName", "ساعات" },
+                                                              { "JustNowPattern", "%u" },
+                                                              { "JustNowFuturePrefix", "" },
+                                                              { "JustNowFutureSuffix", "بعد لحظات" },
+                                                              { "JustNowPastPrefix", "منذ لحظات" },
+                                                              { "JustNowPastSuffix", "" },
+                                                              { "JustNowName", "" },
+                                                              { "JustNowPluralName", "" },
+                                                              { "MillenniumPattern", "%n %u" },
+                                                              { "MillenniumFuturePrefix", "" },
+                                                              { "MillenniumFutureSuffix", " من الآن" },
+                                                              { "MillenniumPastPrefix", "" },
+                                                              { "MillenniumPastSuffix", " مضت" },
+                                                              { "MillenniumName", "جيل" },
+                                                              { "MillenniumPluralName", "اجيال" },
+                                                              { "MillisecondPattern", "%n %u" },
+                                                              { "MillisecondFuturePrefix", "" },
+                                                              { "MillisecondFutureSuffix", " من الآن" },
+                                                              { "MillisecondPastPrefix", "" },
+                                                              { "MillisecondPastSuffix", " مضت" },
+                                                              { "MillisecondName", "جزء من الثانية" },
+                                                              { "MillisecondPluralName", "اجزاء من الثانية" },
+                                                              { "MinutePattern", "%n %u" },
+                                                              { "MinuteFuturePrefix", "" },
+                                                              { "MinuteFutureSuffix", " من الآن" },
+                                                              { "MinutePastPrefix", "" },
+                                                              { "MinutePastSuffix", " مضت" },
+                                                              { "MinuteName", "دقيقة" },
+                                                              { "MinutePluralName", "دقائق" },
+                                                              { "MonthPattern", "%n %u" },
+                                                              { "MonthFuturePrefix", "" },
+                                                              { "MonthFutureSuffix", " من الآن" },
+                                                              { "MonthPastPrefix", "" },
+                                                              { "MonthPastSuffix", " مضت" },
+                                                              { "MonthName", "شهر" },
+                                                              { "MonthPluralName", "أشهر" },
+                                                              { "SecondPattern", "%n %u" },
+                                                              { "SecondFuturePrefix", "" },
+                                                              { "SecondFutureSuffix", " من الآن" },
+                                                              { "SecondPastPrefix", "" },
+                                                              { "SecondPastSuffix", " مضت" },
+                                                              { "SecondName", "ثانية" },
+                                                              { "SecondPluralName", "ثوان" },
+                                                              { "WeekPattern", "%n %u" },
+                                                              { "WeekFuturePrefix", "" },
+                                                              { "WeekFutureSuffix", " من الآن" },
+                                                              { "WeekPastPrefix", "" },
+                                                              { "WeekPastSuffix", " مضت" },
+                                                              { "WeekName", "أسبوع" },
+                                                              { "WeekPluralName", "أسابيع" },
+                                                              { "YearPattern", "%n %u" },
+                                                              { "YearFuturePrefix", "" },
+                                                              { "YearFutureSuffix", " من الآن" },
+                                                              { "YearPastPrefix", "" },
+                                                              { "YearPastSuffix", " مضت" },
+                                                              { "YearName", "سنة" },
+                                                              { "YearPluralName", "سنوات" },
+                                                              { "AbstractTimeUnitPattern", "" },
+                                                              { "AbstractTimeUnitFuturePrefix", "" },
+                                                              { "AbstractTimeUnitFutureSuffix", "" },
+                                                              { "AbstractTimeUnitPastPrefix", "" },
+                                                              { "AbstractTimeUnitPastSuffix", "" },
+                                                              { "AbstractTimeUnitName", "" },
+                                                              { "AbstractTimeUnitPluralName", "" } };
+
+    @Override
+    public Object[][] getContents()
+    {
+        return OBJECTS;
+    }
+
+}

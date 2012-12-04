bdf714941516d2c2324625cc820aef9b944ba101
Mathieu Perez <mathieu.perez@novacodex.net>
2012-12-04 15:09:24 +0100

    translate into catalan (ca_ES)

---
+++ src/main/java/org/ocpsoft/prettytime/i18n/Resources_ca_es.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+public class Resources_ca_es extends ListResourceBundle
+{
+
+    private static final Object[][] OBJECTS = new Object[][] {
+                                                              { "CenturyPattern", "%n %u" },
+                                                              { "CenturyFuturePrefix", "dintre de " },
+                                                              { "CenturyFutureSuffix", "" },
+                                                              { "CenturyPastPrefix", "fa " },
+                                                              { "CenturyPastSuffix", "" },
+                                                              { "CenturyName", "segle" },
+                                                              { "CenturyPluralName", "segles" },
+                                                              { "DayPattern", "%n %u" },
+                                                              { "DayFuturePrefix", "dintre de " },
+                                                              { "DayFutureSuffix", "" },
+                                                              { "DayPastPrefix", "fa " },
+                                                              { "DayPastSuffix", "" },
+                                                              { "DayName", "dia" },
+                                                              { "DayPluralName", "dies" },
+                                                              { "DecadePattern", "%n %u" },
+                                                              { "DecadeFuturePrefix", "dintre de " },
+                                                              { "DecadeFutureSuffix", "" },
+                                                              { "DecadePastPrefix", "fa " },
+                                                              { "DecadePastSuffix", "" },
+                                                              { "DecadeName", "deseni" },
+                                                              { "DecadePluralName", "desenis" },
+                                                              { "HourPattern", "%n %u" },
+                                                              { "HourFuturePrefix", "dintre de " },
+                                                              { "HourFutureSuffix", "" },
+                                                              { "HourPastPrefix", "fa " },
+                                                              { "HourPastSuffix", "" },
+                                                              { "HourName", "hora" },
+                                                              { "HourPluralName", "hores" },
+                                                              { "JustNowPattern", "%u" },
+                                                              { "JustNowFuturePrefix", "" },
+                                                              { "JustNowFutureSuffix", "en un instant" },
+                                                              { "JustNowPastPrefix", "en uns instants" },
+                                                              { "JustNowPastSuffix", "" },
+                                                              { "JustNowName", "" },
+                                                              { "JustNowPluralName", "" },
+                                                              { "MillenniumPattern", "%n %u" },
+                                                              { "MillenniumFuturePrefix", "dintre de " },
+                                                              { "MillenniumFutureSuffix", "" },
+                                                              { "MillenniumPastPrefix", "fa " },
+                                                              { "MillenniumPastSuffix", "" },
+                                                              { "MillenniumName", "mileni" },
+                                                              { "MillenniumPluralName", "milenis" },
+                                                              { "MillisecondPattern", "%n %u" },
+                                                              { "MillisecondFuturePrefix", "dintre de " },
+                                                              { "MillisecondFutureSuffix", "" },
+                                                              { "MillisecondPastPrefix", "fa " },
+                                                              { "MillisecondPastSuffix", "" },
+                                                              { "MillisecondName", "mil·lisegon" },
+                                                              { "MillisecondPluralName", "mil·lisegons" },
+                                                              { "MinutePattern", "%n %u" },
+                                                              { "MinuteFuturePrefix", "dintre de " },
+                                                              { "MinuteFutureSuffix", "" },
+                                                              { "MinutePastPrefix", "fa " },
+                                                              { "MinutePastSuffix", "" },
+                                                              { "MinuteName", "minut" },
+                                                              { "MinutePluralName", "minuts" },
+                                                              { "MonthPattern", "%n %u" },
+                                                              { "MonthFuturePrefix", "dintre de " },
+                                                              { "MonthFutureSuffix", "" },
+                                                              { "MonthPastPrefix", "fa " },
+                                                              { "MonthPastSuffix", "" },
+                                                              { "MonthName", "mes" },
+                                                              { "MonthPluralName", "mesos" },
+                                                              { "SecondPattern", "%n %u" },
+                                                              { "SecondFuturePrefix", "dintre de " },
+                                                              { "SecondFutureSuffix", "" },
+                                                              { "SecondPastPrefix", "fa " },
+                                                              { "SecondPastSuffix", "" },
+                                                              { "SecondName", "segon" },
+                                                              { "SecondPluralName", "segons" },
+                                                              { "WeekPattern", "%n %u" },
+                                                              { "WeekFuturePrefix", "dintre de " },
+                                                              { "WeekFutureSuffix", "" },
+                                                              { "WeekPastPrefix", "fa " },
+                                                              { "WeekPastSuffix", "" },
+                                                              { "WeekName", "setmana" },
+                                                              { "WeekPluralName", "setmanes" },
+                                                              { "YearPattern", "%n %u" },
+                                                              { "YearFuturePrefix", "dintre de " },
+                                                              { "YearFutureSuffix", "" },
+                                                              { "YearPastPrefix", "fa " },
+                                                              { "YearPastSuffix", "" },
+                                                              { "YearName", "any" },
+                                                              { "YearPluralName", "anys" },
+                                                              { "AbstractTimeUnitPattern", "" },
+                                                              { "AbstractTimeUnitFuturePrefix", "" },
+                                                              { "AbstractTimeUnitFutureSuffix", "" },
+                                                              { "AbstractTimeUnitPastPrefix", "" },
+                                                              { "AbstractTimeUnitPastSuffix", "" },
+                                                              { "AbstractTimeUnitName", "" },
+                                                              { "AbstractTimeUnitPluralName", "" } };
+
+
+    @Override
+    protected Object[][] getContents()
+    {
+        return OBJECTS;
+    }
+}
---
+++ src/test/java/org/ocpsoft/prettytime/PrettyTimeI18n_CA_ES_Test.java
@@ -0,0 +1,293 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.ocpsoft.prettytime.format.SimpleTimeFormat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PrettyTimeI18n_CA_ES_Test
+{
+   SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+
+   // Stores current locale so that it can be restored
+   protected Locale locale;
+
+   // Method setUp() is called automatically before every test method
+   @Before
+   public void setUp() throws Exception
+   {
+      locale = new Locale( "ca_ES" );
+   }
+
+   @Test
+   public void testCeilingInterval() throws Exception
+   {
+      Date then = format.parse("5/20/2009");
+      Date ref = format.parse("6/17/2009");
+      PrettyTime t = new PrettyTime(ref, new Locale("ca_ES"));
+      assertEquals("fa 1 mes", t.format(then));
+   }
+
+   @Test
+   public void testNullDate() throws Exception
+   {
+      PrettyTime t = new PrettyTime();
+      Date date = null;
+      assertEquals("en un instant", t.format(date));
+   }
+
+   @Test
+   public void testRightNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime();
+      assertEquals("en un instant", t.format(new Date()));
+   }
+
+   @Test
+   public void testRightNowVariance() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0));
+      assertEquals("en un instant", t.format(new Date(600)));
+   }
+
+   @Test
+   public void testMinutesFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0));
+      assertEquals("dintre de 12 minuts", t.format(new Date(1000 * 60 * 12)));
+   }
+
+   @Test
+   public void testHoursFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0));
+      assertEquals("dintre de 3 hores", t.format(new Date(1000 * 60 * 60 * 3)));
+   }
+
+   @Test
+   public void testDaysFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0));
+      assertEquals("dintre de 3 dies", t.format(new Date(1000 * 60 * 60 * 24 * 3)));
+   }
+
+   @Test
+   public void testWeeksFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0));
+      assertEquals("dintre de 3 setmanes", t.format(new Date(1000 * 60 * 60 * 24 * 7 * 3)));
+   }
+
+   @Test
+   public void testMonthsFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0));
+      assertEquals("dintre de 3 mesos", t.format(new Date(2629743830L * 3L)));
+   }
+
+   @Test
+   public void testYearsFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0));
+      assertEquals("dintre de 3 anys", t.format(new Date(2629743830L * 12L * 3L)));
+   }
+
+   @Test
+   public void testDecadesFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0));
+      assertEquals("dintre de 3 desenis", t.format(new Date(315569259747L * 3L)));
+   }
+
+   @Test
+   public void testCenturiesFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0));
+      assertEquals("dintre de 3 segles", t.format(new Date(3155692597470L * 3L)));
+   }
+
+   /*
+    * Past
+    */
+   @Test
+   public void testMomentsAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(6000));
+      assertEquals("en uns instants", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testMinutesAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(1000 * 60 * 12));
+      assertEquals("fa 12 minuts", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testHoursAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 3));
+      assertEquals("fa 3 hores", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testDaysAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 24 * 3));
+      assertEquals("fa 3 dies", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testWeeksAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 24 * 7 * 3));
+      assertEquals("fa 3 setmanes", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testMonthsAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(2629743830L * 3L));
+      assertEquals("fa 3 mesos", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testCustomFormat() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0));
+      TimeUnit unit = new TimeUnit()
+      {
+         @Override
+         public long getMaxQuantity()
+         {
+            return 0;
+         }
+
+         @Override
+         public long getMillisPerUnit()
+         {
+            return 5000;
+         }
+      };
+      t.clearUnits();
+      t.registerUnit(unit, new SimpleTimeFormat()
+      .setName("tick").setPluralName("ticks")
+      .setPattern("%n %u").setRoundingTolerance(20)
+      .setFutureSuffix("... RUN!")
+      .setFuturePrefix("self destruct in: ").setPastPrefix("self destruct was: ").setPastSuffix(
+               " ago..."));
+
+      assertEquals("self destruct in: 5 ticks ... RUN!", t.format(new Date(25000)));
+      t.setReference(new Date(25000));
+      assertEquals("self destruct was: 5 ticks ago...", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testYearsAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(2629743830L * 12L * 3L));
+      assertEquals("fa 3 anys", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testDecadesAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(315569259747L * 3L));
+      assertEquals("fa 3 desenis", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testCenturiesAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(3155692597470L * 3L));
+      assertEquals("fa 3 segles", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testWithinTwoHoursRounding() throws Exception
+   {
+      PrettyTime t = new PrettyTime();
+      assertEquals("fa 2 hores", t.format(new Date(new Date().getTime() - 6543990)));
+   }
+
+   @Test
+   public void testPreciseInTheFuture() throws Exception
+   {
+      PrettyTime t = new PrettyTime();
+      List<Duration> durations = t.calculatePreciseDuration(new Date(new Date().getTime() + 1000
+               * (10 * 60 + 5 * 60 * 60)));
+      assertTrue(durations.size() >= 2); // might be more because of milliseconds between date capturing and result
+      // calculation
+      assertEquals(5, durations.get(0).getQuantity());
+      assertEquals(10, durations.get(1).getQuantity());
+   }
+
+   @Test
+   public void testPreciseInThePast() throws Exception
+   {
+      PrettyTime t = new PrettyTime();
+      List<Duration> durations = t.calculatePreciseDuration(new Date(new Date().getTime() - 1000
+               * (10 * 60 + 5 * 60 * 60)));
+      assertTrue(durations.size() >= 2); // might be more because of milliseconds between date capturing and result
+      // calculation
+      assertEquals(-5, durations.get(0).getQuantity());
+      assertEquals(-10, durations.get(1).getQuantity());
+   }
+
+   @Test
+   public void testFormattingDurationListInThePast() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 24 * 3 + 1000 * 60 * 60 * 15 + 1000 * 60 * 38));
+      List<Duration> durations = t.calculatePreciseDuration(new Date(0));
+      assertEquals("fa 3 dies 15 hores 38 minuts", t.format(durations));
+   }
+
+   @Test
+   public void testFormattingDurationListInTheFuture() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0));
+      List<Duration> durations = t.calculatePreciseDuration(new Date(1000 * 60 * 60 * 24 * 3 + 1000 * 60 * 60 * 15
+               + 1000 * 60 * 38));
+      assertEquals("dintre de 3 dies 15 hores 38 minuts", t.format(durations));
+   }
+
+   @Test
+   public void testSetLocale() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(315569259747L * 3L));
+      assertEquals("fa 3 desenis", t.format(new Date(0)));
+      t.setLocale(Locale.GERMAN);
+      assertEquals("vor 3 Jahrzehnten", t.format(new Date(0)));
+   }
+
+   // Method tearDown() is called automatically after every test method
+   @After
+   public void tearDown() throws Exception
+   {
+      Locale.setDefault(locale);
+   }
+
+}

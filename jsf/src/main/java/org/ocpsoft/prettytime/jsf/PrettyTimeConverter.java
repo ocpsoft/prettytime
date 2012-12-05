2d9576a6a460b96952f8c4bd7ef0e84ff61f7da7
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2012-12-05 13:39:04 -0500

    New project structure to remove unnecessary deps/code

--- jsf/src/main/java/org/ocpsoft/prettytime/jsf/PrettyTimeConverter.java
+++ jsf/src/main/java/org/ocpsoft/prettytime/jsf/PrettyTimeConverter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.ocpsoft.prettytime.web.jsf;
+package org.ocpsoft.prettytime.jsf;
 
 
 import javax.faces.component.UIComponent;

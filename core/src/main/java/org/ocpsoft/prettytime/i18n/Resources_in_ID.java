56ed28e15b44eb3285946103ce3c75c7eedb2330
xsalefter <xsalefter@gmail.com>
2012-12-08 09:40:08 +0700

    Initial commit for in_ID (Indonesian) Locale.

---
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_in_ID.java
@@ -0,0 +1,112 @@
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+/**
+ * Resource for Indonesian language.
+ * 
+ * @author xsalefter (xsalefter[at]gmail.com)
+ */
+public class Resources_in_ID extends ListResourceBundle {
+
+	private static final Object[][] OBJECTS = new Object[][] {
+		{ "CenturyPattern", "%n %u" },
+        { "CenturyFuturePrefix", "" },
+        { "CenturyFutureSuffix", " dari sekarang" },
+        { "CenturyPastPrefix", "" },
+        { "CenturyPastSuffix", " yang lalu" },
+        { "CenturySingularName", "abad" },
+        { "CenturyPluralName", "abad" },
+        { "DayPattern", "%n %u" },
+        { "DayFuturePrefix", "" },
+        { "DayFutureSuffix", " dari sekarang" },
+        { "DayPastPrefix", "" },
+        { "DayPastSuffix", " yang lalu" },
+        { "DaySingularName", "hari" },
+        { "DayPluralName", "hari" },
+        { "DecadePattern", "%n %u" },
+        { "DecadeFuturePrefix", "" },
+        { "DecadeFutureSuffix", " dari sekarang" },
+        { "DecadePastPrefix", "" },
+        { "DecadePastSuffix", " yang lalu" },
+        { "DecadeSingularName", "dekade" },
+        { "DecadePluralName", "dekade" },
+        { "HourPattern", "%n %u" },
+        { "HourFuturePrefix", "" },
+        { "HourFutureSuffix", " dari sekarang" },
+        { "HourPastPrefix", "" },
+        { "HourPastSuffix", " yang lalu" },
+        { "HourSingularName", "jam" },
+        { "HourPluralName", "jam" },
+        { "JustNowPattern", "%u" },
+        { "JustNowFuturePrefix", "" },
+        { "JustNowFutureSuffix", "dari sekarang" },
+        { "JustNowPastPrefix", "yang lalu" },
+        { "JustNowPastSuffix", "" },
+        { "JustNowSingularName", "" },
+        { "JustNowPluralName", "" },
+        { "MillenniumPattern", "%n %u" },
+        { "MillenniumFuturePrefix", "" },
+        { "MillenniumFutureSuffix", " dari sekarang" },
+        { "MillenniumPastPrefix", "" },
+        { "MillenniumPastSuffix", " yang lalu" },
+        { "MillenniumSingularName", "ribuan tahun" },
+        { "MillenniumPluralName", "ribuan tahun" },
+        { "MillisecondPattern", "%n %u" },
+        { "MillisecondFuturePrefix", "" },
+        { "MillisecondFutureSuffix", " dari sekarang" },
+        { "MillisecondPastPrefix", "" },
+        { "MillisecondPastSuffix", " yang lalu" },
+        { "MillisecondSingularName", "mili detik" },
+        { "MillisecondPluralName", "mili detik" },
+        { "MinutePattern", "%n %u" },
+        { "MinuteFuturePrefix", "" },
+        { "MinuteFutureSuffix", " dari sekarang" },
+        { "MinutePastPrefix", "" },
+        { "MinutePastSuffix", " yang lalu" },
+        { "MinuteSingularName", "menit" },
+        { "MinutePluralName", "menit" },
+        { "MonthPattern", "%n %u" },
+        { "MonthFuturePrefix", "" },
+        { "MonthFutureSuffix", " dari sekarang" },
+        { "MonthPastPrefix", "" },
+        { "MonthPastSuffix", " yang lalu" },
+        { "MonthSingularName", "bulan" },
+        { "MonthPluralName", "bulan" },
+        { "SecondPattern", "%n %u" },
+        { "SecondFuturePrefix", "" },
+        { "SecondFutureSuffix", " dari sekarang" },
+        { "SecondPastPrefix", "" },
+        { "SecondPastSuffix", " yang lalu" },
+        { "SecondSingularName", "detik" },
+        { "SecondPluralName", "detik" },
+        { "WeekPattern", "%n %u" },
+        { "WeekFuturePrefix", "" },
+        { "WeekFutureSuffix", " dari sekarang" },
+        { "WeekPastPrefix", "" },
+        { "WeekPastSuffix", " yang lalu" },
+        { "WeekSingularName", "minggu" },
+        { "WeekPluralName", "minggu" },
+        { "YearPattern", "%n %u" },
+        { "YearFuturePrefix", "" },
+        { "YearFutureSuffix", " dari sekarang" },
+        { "YearPastPrefix", "" },
+        { "YearPastSuffix", " yang lalu" },
+        { "YearSingularName", "tahun" },
+        { "YearPluralName", "tahun" },
+        { "AbstractTimeUnitPattern", "" },
+        { "AbstractTimeUnitFuturePrefix", "" },
+        { "AbstractTimeUnitFutureSuffix", "" },
+        { "AbstractTimeUnitPastPrefix", "" },
+        { "AbstractTimeUnitPastSuffix", "" },
+        { "AbstractTimeUnitSingularName", "" },
+        { "AbstractTimeUnitPluralName", "" }
+	};
+
+	@Override
+	protected Object[][] getContents() 
+	{
+		return OBJECTS;
+	}
+
+}

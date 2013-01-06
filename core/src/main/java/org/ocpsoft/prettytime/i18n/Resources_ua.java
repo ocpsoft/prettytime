2a1bf035730f1f8e2c7b9947ef6a121c10775717
452 <sp.titan@gmail.com>
2013-01-06 15:12:57 +0200

    added Ukrainian language from Tumin Alexander implementation

---
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_ua.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime.i18n;
+
+import org.ocpsoft.prettytime.Duration;
+import org.ocpsoft.prettytime.TimeFormat;
+import org.ocpsoft.prettytime.TimeUnit;
+import org.ocpsoft.prettytime.impl.TimeFormatProvider;
+import org.ocpsoft.prettytime.units.*;
+
+import java.util.ListResourceBundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tumin Alexander
+ * Date: 2012-12-13
+ * Time: 03:33
+ * 
+ * reedit to Ukrainian with Eclipse).
+ * User: Ihor Lavrynuk
+ * Date: 2013-01-06
+ * Time: 15:04
+ * 
+ */
+public class Resources_ua extends ListResourceBundle implements TimeFormatProvider
+{
+    private static final Object[][] OBJECTS = new Object[0][0];
+
+    private static final int tolerance = 50;
+
+    // see http://translate.sourceforge.net/wiki/l10n/pluralforms
+    private static final int slavicPluralForms = 3;
+
+    private static class TimeFormatAided implements TimeFormat {
+        private final String[] pluarls;
+
+        public TimeFormatAided(String ... plurals) {
+            if (plurals.length != slavicPluralForms) {
+                throw new IllegalArgumentException("Wrong plural forms number for slavic language!");
+            }
+            this.pluarls = plurals;
+        }
+
+        @Override
+        public String format(Duration duration) {
+            long quantity = duration.getQuantityRounded(tolerance);
+            StringBuilder result = new StringBuilder();
+            result.append(quantity);
+            return result.toString();
+        }
+
+        @Override
+        public String formatUnrounded(Duration duration) {
+            long quantity = duration.getQuantity();
+            StringBuilder result = new StringBuilder();
+            result.append(quantity);
+            return result.toString();
+        }
+
+        @Override
+        public String decorate(Duration duration, String time) {
+            return performDecoration(
+                    duration.isInPast(),
+                    duration.isInFuture(),
+                    duration.getQuantityRounded(tolerance),
+                    time
+            );
+        }
+
+        @Override
+        public String decorateUnrounded(Duration duration, String time) {
+            return performDecoration(
+                    duration.isInPast(),
+                    duration.isInFuture(),
+                    duration.getQuantity(),
+                    time
+            );
+        }
+
+        private String performDecoration(boolean past, boolean future, long n, String time) {
+            // a bit cryptic, yet well-tested
+            // consider http://translate.sourceforge.net/wiki/l10n/pluralforms
+            int pluralIdx = (n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);
+            if (pluralIdx > slavicPluralForms) {
+                // impossible happening
+                throw new IllegalStateException("Wrong plural index was calculated somehow for slavic language");
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            if (future) {
+                result.append("через ");
+            }
+
+            result.append(time);
+            result.append(' ');
+            result.append(pluarls[pluralIdx]);
+
+            if (past) {
+                result.append(" тому");
+            }
+
+            return result.toString();
+        }
+    }
+
+    @Override
+    public Object[][] getContents()
+    {
+        return OBJECTS;
+    }
+
+    @Override
+    public TimeFormat getFormatFor(TimeUnit t) {
+        if (t instanceof JustNow) {
+            return new TimeFormat() {
+                @Override
+                public String format(Duration duration) {
+                    return performFormat(duration);
+                }
+
+                @Override
+                public String formatUnrounded(Duration duration) {
+                    return performFormat(duration);
+                }
+
+                private String performFormat(Duration duration) {
+                    if (duration.isInFuture()) {
+                        return "зараз";
+                    }
+                    if (duration.isInPast()) {
+                        return "тільки що";
+                    }
+                    return null;
+                }
+
+                @Override
+                public String decorate(Duration duration, String time) {
+                    return time;
+                }
+
+                @Override
+                public String decorateUnrounded(Duration duration, String time) {
+                    return time;
+                }
+            };
+        } else if (t instanceof Century) {
+            return new TimeFormatAided("століття", "століття", "столітть");
+        } else if (t instanceof Day) {
+            return new TimeFormatAided("день", "дні", "днів");
+        } else if (t instanceof Decade) {
+            return new TimeFormatAided("десятиліття", "десятиліття", "десятиліть");
+        } else if (t instanceof Hour) {
+            return new TimeFormatAided("година", "години", "годин");
+        } else if (t instanceof Millennium) {
+            return new TimeFormatAided("тисячоліття", "тисячоліття", "тисячоліть");
+        } else if (t instanceof Millisecond) {
+            return new TimeFormatAided("мілісекунду", "мілісекунди", "мілісекунд");
+        } else if (t instanceof Minute) {
+            return new TimeFormatAided("хвилину", "хвилини", "хвилин");
+        } else if (t instanceof Month) {
+            return new TimeFormatAided("місяць", "місяці", "місяців");
+        } else if (t instanceof Second) {
+            return new TimeFormatAided("секунду", "секунди", "секунд");
+        } else if (t instanceof Week) {
+            return new TimeFormatAided("тиждень", "тижні", "тижнів");
+        } else if (t instanceof Year) {
+            return new TimeFormatAided("рік", "роки", "рік");
+        }
+        return null; // error
+    }
+}
---
+++ core/src/test/java/org/ocpsoft/prettytime/PrettyTimeI18n_UA_Test.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * User: Ihor Lavrynuk
+ * Date: 2013-01-05
+ * Time: 16:57
+ */
+public class PrettyTimeI18n_UA_Test {
+    private final SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+    private Locale locale;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        locale = new Locale("ua");
+        Locale.setDefault(locale);
+    }
+
+    @Test
+    public void testPrettyTime()
+    {
+        PrettyTime p = new PrettyTime(locale);
+        assertEquals("зараз", p.format(new Date()));
+    }
+
+    @Test
+    public void testPrettyTimeCenturies()
+    {
+        PrettyTime p = new PrettyTime(new Date(3155692597470L * 3L), locale);
+        assertEquals("3 століття тому", p.format(new Date(0)));
+
+        p = new PrettyTime(new Date(0), locale);
+        assertEquals("через 3 століття", p.format(new Date(3155692597470L * 3L)));
+    }
+
+    @Test
+    public void testCeilingInterval() throws Exception
+    {
+        Date then = format.parse("20/5/2009");
+        Date ref = format.parse("17/6/2009");
+        PrettyTime t = new PrettyTime(ref, locale);
+        assertEquals("1 місяць тому", t.format(then));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullDate() throws Exception
+    {
+        PrettyTime t = new PrettyTime(locale);
+        Date date = null;
+        assertEquals("зараз", t.format(date));
+    }
+
+    @Test
+    public void testRightNow() throws Exception
+    {
+        PrettyTime t = new PrettyTime(locale);
+        assertEquals("зараз", t.format(new Date()));
+    }
+
+    @Test
+    public void testRightNowVariance() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(0), locale);
+        assertEquals("зараз", t.format(new Date(600)));
+    }
+
+    @Test
+    public void testMinutesFromNow() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(0), locale);
+        assertEquals("через 12 хвилин", t.format(new Date(1000 * 60 * 12)));
+    }
+
+    @Test
+    public void testHoursFromNow() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(0), locale);
+        assertEquals("через 3 години", t.format(new Date(1000 * 60 * 60 * 3)));
+    }
+
+    @Test
+    public void testDaysFromNow() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(0), locale);
+        assertEquals("через 3 дні", t.format(new Date(1000 * 60 * 60 * 24 * 3)));
+    }
+ 
+    @Test
+    public void testWeeksFromNow() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(0), locale);
+        assertEquals("через 3 тижні", t.format(new Date(1000 * 60 * 60 * 24 * 7 * 3)));
+    }
+
+    @Test
+    public void testMonthsFromNow() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(0), locale);
+        assertEquals("через 3 місяці", t.format(new Date(2629743830L * 3L)));
+    }
+
+    @Test
+    public void testYearsFromNow() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(0), locale);
+        assertEquals("через 3 роки", t.format(new Date(2629743830L * 12L * 3L)));
+    }
+
+    @Test
+    public void testDecadesFromNow() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(0), locale);
+        assertEquals("через 3 десятиліття", t.format(new Date(315569259747L * 3L)));
+    }
+
+    @Test
+    public void testCenturiesFromNow() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(0), locale);
+        assertEquals("через 3 століття", t.format(new Date(3155692597470L * 3L)));
+    }
+
+    /*
+     * Past
+     */
+    @Test
+    public void testMomentsAgo() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(6000), locale);
+        assertEquals("тільки що", t.format(new Date(0)));
+    }
+
+    @Test
+    public void testMinutesAgo() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(1000 * 60 * 12), locale);
+        assertEquals("12 хвилин тому", t.format(new Date(0)));
+    }
+
+    @Test
+    public void testHoursAgo() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 3), locale);
+        assertEquals("3 години тому", t.format(new Date(0)));
+    }
+
+    @Test
+    public void testDaysAgo() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 24 * 3), locale);
+        assertEquals("3 дні тому", t.format(new Date(0)));
+    }
+
+    @Test
+    public void testWeeksAgo() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 24 * 7 * 3), locale);
+        assertEquals("3 тижні тому", t.format(new Date(0)));
+    }
+
+    @Test
+    public void testMonthsAgo() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(2629743830L * 3L), locale);
+        assertEquals("3 місяці тому", t.format(new Date(0)));
+    }
+
+    @Test
+    public void testYearsAgo() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(2629743830L * 12L * 3L), locale);
+        assertEquals("3 роки тому", t.format(new Date(0)));
+    }
+
+    @Test
+    public void testDecadesAgo() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(315569259747L * 3L), locale);
+        assertEquals("3 десятиліття тому", t.format(new Date(0)));
+    }
+
+    @Test
+    public void testCenturiesAgo() throws Exception
+    {
+        PrettyTime t = new PrettyTime(new Date(3155692597470L * 3L), locale);
+        assertEquals("3 століття тому", t.format(new Date(0)));
+    }
+
+
+    @After
+    public void tearDown() throws Exception
+    {
+        Locale.setDefault(locale);
+    }
+}

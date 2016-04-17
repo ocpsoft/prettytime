84c991336bd2cd54243c544dcea09fc8bc1d3119
xirwajim <xirwajim@qq.com>
2016-04-17 23:06:03 +0800

    i18n for uyghur

---
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_uy.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+public class Resources_uy extends ListResourceBundle
+{
+
+   private static final Object[][] OBJECTS = new Object[][] {
+            { "CenturyPattern", "%n %u" },
+            { "CenturyFuturePrefix", "" },
+            { "CenturyFutureSuffix", "كىيىن" },
+            { "CenturyPastPrefix", "" },
+            { "CenturyPastSuffix", "ئىلگىرى" },
+            { "CenturySingularName", "ئەسىر" },
+            { "CenturyPluralName", "ئەسىر" },
+            { "DayPattern", "%n %u" },
+            { "DayFuturePrefix", "" },
+            { "DayFutureSuffix", "كىيىن" },
+            { "DayPastPrefix", "" },
+            { "DayPastSuffix", "ئىلگىرى" },
+            { "DaySingularName", "كۈن" },
+            { "DayPluralName", "كۈن" },
+            { "DecadePattern", "%n %u" },
+            { "DecadeFuturePrefix", "" },
+            { "DecadeFutureSuffix", "كىيىن" },
+            { "DecadePastPrefix", "" },
+            { "DecadePastSuffix", "ئىلگىرى" },
+            { "DecadeSingularName", "0 يىل" },
+            { "DecadePluralName", "0 يىل" },
+            { "HourPattern", "%n %u" },
+            { "HourFuturePrefix", "" },
+            { "HourFutureSuffix", "كىيىن" },
+            { "HourPastPrefix", "" },
+            { "HourPastSuffix", "ئىلگىرى" },
+            { "HourSingularName", "سائەت" },
+            { "HourPluralName", "سائەت" },
+            { "JustNowPattern", "%u" },
+            { "JustNowFuturePrefix", "" },
+            { "JustNowFutureSuffix", "ھېلىلا" },
+            { "JustNowPastPrefix", "ھېلىلا" },
+            { "JustNowPastSuffix", "" },
+            { "JustNowSingularName", "" },
+            { "JustNowPluralName", "" },
+            { "MillenniumPattern", "%n %u" },
+            { "MillenniumFuturePrefix", "" },
+            { "MillenniumFutureSuffix", "كىيىن" },
+            { "MillenniumPastPrefix", "" },
+            { "MillenniumPastSuffix", "ئىلگىرى" },
+            { "MillenniumSingularName", "مىڭ يىل" },
+            { "MillenniumPluralName", "مىڭ يىل" },
+            { "MillisecondPattern", "%n %u" },
+            { "MillisecondFuturePrefix", "" },
+            { "MillisecondFutureSuffix", "كىيىن" },
+            { "MillisecondPastPrefix", "" },
+            { "MillisecondPastSuffix", "ئىلگىرى" },
+            { "MillisecondSingularName", "دەقىقە" },
+            { "MillisecondPluralName", "دەقىقە" },
+            { "MinutePattern", "%n %u" },
+            { "MinuteFuturePrefix", "" },
+            { "MinuteFutureSuffix", "كىيىن" },
+            { "MinutePastPrefix", "" },
+            { "MinutePastSuffix", "ئىلگىرى" },
+            { "MinuteSingularName", "مىنۇت" },
+            { "MinutePluralName", "مىنۇت" },
+            { "MonthPattern", "%n %u" },
+            { "MonthFuturePrefix", "" },
+            { "MonthFutureSuffix", "كىيىن" },
+            { "MonthPastPrefix", "" },
+            { "MonthPastSuffix", "ئىلگىرى" },
+            { "MonthSingularName", "ئاي" },
+            { "MonthPluralName", "ئاي" },
+            { "SecondPattern", "%n %u" },
+            { "SecondFuturePrefix", "" },
+            { "SecondFutureSuffix", "كىيىن" },
+            { "SecondPastPrefix", "" },
+            { "SecondPastSuffix", "ئىلگىرى" },
+            { "SecondSingularName", "سېكۇنت" },
+            { "SecondPluralName", "سېكۇنت" },
+            { "WeekPattern", "%n %u" },
+            { "WeekFuturePrefix", "" },
+            { "WeekFutureSuffix", "كىيىن" },
+            { "WeekPastPrefix", "" },
+            { "WeekPastSuffix", "ئىلگىرى" },
+            { "WeekSingularName", "ھەپتە" },
+            { "WeekPluralName", "ھەپتە" },
+            { "YearPattern", "%n %u" },
+            { "YearFuturePrefix", "" },
+            { "YearFutureSuffix", "كىيىن" },
+            { "YearPastPrefix", "" },
+            { "YearPastSuffix", "ئىلگىرى" },
+            { "YearSingularName", "يىل" },
+            { "YearPluralName", "يىل" },
+            { "AbstractTimeUnitPattern", "" },
+            { "AbstractTimeUnitFuturePrefix", "" },
+            { "AbstractTimeUnitFutureSuffix", "" },
+            { "AbstractTimeUnitPastPrefix", "" },
+            { "AbstractTimeUnitPastSuffix", "" },
+            { "AbstractTimeUnitSingularName", "" },
+            { "AbstractTimeUnitPluralName", "" } };
+
+   @Override
+   protected Object[][] getContents()
+   {
+      return OBJECTS;
+   }
+}

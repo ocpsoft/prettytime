2ae104eec52b845457c7ede12ee58966bee53674
xirwajim <xirwajim@qq.com>
2016-04-17 23:06:37 +0800

    test for uyghur

---
+++ core/src/test/java/org/ocpsoft/prettytime/PrettyTimeI18n_uy_Test.java
@@ -0,0 +1,231 @@
+package org.ocpsoft.prettytime;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by edward_chiang on 13/6/27.
+ */
+public class PrettyTimeI18n_uy_Test
+{
+   private final SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+   private Locale locale;
+
+   @Before
+   public void setUp() throws Exception
+   {
+      locale = new Locale("uy");
+      Locale.setDefault(locale);
+   }
+
+   @Test
+   public void testPrettyTime()
+   {
+      PrettyTime p = new PrettyTime(locale);
+      assertEquals("ھېلىلا", p.format(new Date()));
+   }
+
+   @Test
+   public void testPrettyTimeCenturies()
+   {
+      PrettyTime p = new PrettyTime(new Date(3155692597470L * 3L), locale);
+      assertEquals("3 ئەسىر ئىلگىرى", p.format(new Date(0)));
+
+      p = new PrettyTime(new Date(0), locale);
+      assertEquals("3 ئەسىر كىيىن", p.format(new Date(3155692597470L * 3L)));
+   }
+
+   @Test
+   public void testCeilingInterval() throws Exception
+   {
+      Date then = format.parse("20/5/2009");
+      Date ref = format.parse("17/6/2009");
+      PrettyTime t = new PrettyTime(ref, locale);
+      assertEquals("1 ئاي ئىلگىرى", t.format(then));
+   }
+
+   @Test
+   public void testNullDate() throws Exception
+   {
+      PrettyTime t = new PrettyTime(locale);
+      Date date = null;
+      assertEquals("ھېلىلا", t.format(date));
+   }
+
+   @Test
+   public void testRightNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(locale);
+      assertEquals("ھېلىلا", t.format(new Date()));
+   }
+
+   @Test
+   public void testRightNowVariance() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0), locale);
+      assertEquals("ھېلىلا", t.format(new Date(600)));
+   }
+
+   @Test
+   public void testMinutesFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0), locale);
+      assertEquals("12 مىنۇت كىيىن", t.format(new Date(1000 * 60 * 12)));
+   }
+
+   @Test
+   public void testHoursFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0), locale);
+      assertEquals("3 سائەت كىيىن", t.format(new Date(1000 * 60 * 60 * 3)));
+   }
+
+   @Test
+   public void testDaysFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0), locale);
+      assertEquals("3 كۈن كىيىن", t.format(new Date(1000 * 60 * 60 * 24 * 3)));
+   }
+
+   @Test
+   public void testWeeksFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0), locale);
+      assertEquals("3 ھەپتە كىيىن", t.format(new Date(1000 * 60 * 60 * 24 * 7 * 3)));
+   }
+
+   @Test
+   public void testMonthsFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0), locale);
+      assertEquals("3 ئاي كىيىن", t.format(new Date(2629743830L * 3L)));
+   }
+
+   @Test
+   public void testYearsFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0), locale);
+      assertEquals("3 يىل كىيىن", t.format(new Date(2629743830L * 12L * 3L)));
+   }
+
+   @Test
+   public void testDecadesFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0), locale);
+      assertEquals("3 0 يىل كىيىن", t.format(new Date(315569259747L * 3L)));
+   }
+
+   @Test
+   public void testCenturiesFromNow() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(0), locale);
+      assertEquals("3 ئەسىر كىيىن", t.format(new Date(3155692597470L * 3L)));
+   }
+
+   /*
+    * Past
+    */
+   @Test
+   public void testMomentsAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(6000), locale);
+      assertEquals("ھېلىلا", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testMinutesAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(1000 * 60 * 12), locale);
+      assertEquals("12 مىنۇت ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @Test
+   public void test1HourAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 1), locale);
+      assertEquals("1 سائەت ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @Test
+   public void test3HoursAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 3), locale);
+      assertEquals("3 سائەت ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @Test
+   public void test6HoursAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 6), locale);
+      assertEquals("6 سائەت ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testDaysAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 24 * 3), locale);
+      assertEquals("3 كۈن ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testWeeksAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 24 * 7 * 3), locale);
+      assertEquals("3 ھەپتە ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testMonthsAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(2629743830L * 3L), locale);
+      assertEquals("3 ئاي ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testYearsAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(2629743830L * 12L * 3L), locale);
+      assertEquals("3 يىل ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @Test
+   public void test8YearsAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(2629743830L * 12L * 8L), locale);
+      assertEquals("8 يىل ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testDecadesAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(315569259747L * 3L), locale);
+      assertEquals("3 0 يىل ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @Test
+   public void test8DecadesAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(315569259747L * 8L), locale);
+      assertEquals("8 0 يىل ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @Test
+   public void testCenturiesAgo() throws Exception
+   {
+      PrettyTime t = new PrettyTime(new Date(3155692597470L * 3L), locale);
+      assertEquals("3 ئەسىر ئىلگىرى", t.format(new Date(0)));
+   }
+
+   @After
+   public void tearDown() throws Exception
+   {
+      Locale.setDefault(locale);
+   }
+}

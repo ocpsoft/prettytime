432710a07299572e7fe486fcc7140685523aa430
tobiaszolejnik <tobiasz.olejnik@gmail.com>
2017-05-30 11:30:52 +0200

    Add files via upload
    Khmer language support

---
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_km.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+public class Resources_km extends ListResourceBundle {
+
+    private static final Object[][] OBJECTS = new Object[][]{
+            {"CenturyPattern", "%n %u"},
+            {"CenturyFuturePrefix", ""},
+            {"CenturyFutureSuffix", " ពីឥឡូវនេះ"},
+            {"CenturyPastPrefix", ""},
+            {"CenturyPastSuffix", " មុន"},
+            {"CenturySingularName", "សតវត្ស"},
+            {"CenturyPluralName", "សតវត្ស"},
+            {"DayPattern", "%n %u"},
+            {"DayFuturePrefix", ""},
+            {"DayFutureSuffix", " ពីឥឡូវនេះ"},
+            {"DayPastPrefix", ""},
+            {"DayPastSuffix", " មុន"},
+            {"DaySingularName", "ថ្ងៃ"},
+            {"DayPluralName", "ថ្ងៃ"},
+            {"DecadePattern", "%n %u"},
+            {"DecadeFuturePrefix", ""},
+            {"DecadeFutureSuffix", " ពីឥឡូវនេះ"},
+            {"DecadePastPrefix", ""},
+            {"DecadePastSuffix", " មុន"},
+            {"DecadeSingularName", "ទសវត្សរ៍"},
+            {"DecadePluralName", "ទសវត្សរ៍"},
+            {"HourPattern", "%n %u"},
+            {"HourFuturePrefix", ""},
+            {"HourFutureSuffix", " ពីឥឡូវនេះ"},
+            {"HourPastPrefix", ""},
+            {"HourPastSuffix", " មុន"},
+            {"HourSingularName", "ម៉ោង"},
+            {"HourPluralName", "ម៉ោង"},
+            {"JustNowPattern", "%u"},
+            {"JustNowFuturePrefix", ""},
+            {"JustNowFutureSuffix", "បន្តិចតិចនេះ"},
+            {"JustNowPastPrefix", "មុននេះបន្តិច"},
+            {"JustNowPastSuffix", ""},
+            {"JustNowSingularName", ""},
+            {"JustNowPluralName", ""},
+            {"MillenniumPattern", "%n %u"},
+            {"MillenniumFuturePrefix", ""},
+            {"MillenniumFutureSuffix", " ពីឥឡូវនេះ"},
+            {"MillenniumPastPrefix", ""},
+            {"MillenniumPastSuffix", " មុន"},
+            {"MillenniumSingularName", "សហស្សវត្ស៍"},
+            {"MillenniumPluralName", "សហស្សវត្ស៍"},
+            {"MillisecondPattern", "%n %u"},
+            {"MillisecondFuturePrefix", ""},
+            {"MillisecondFutureSuffix", " ពីឥឡូវនេះ"},
+            {"MillisecondPastPrefix", ""},
+            {"MillisecondPastSuffix", " មុន"},
+            {"MillisecondSingularName", "មីលីវិនាទី​"},
+            {"MillisecondPluralName", "មីលីវិនាទី​"},
+            {"MinutePattern", "%n %u"},
+            {"MinuteFuturePrefix", ""},
+            {"MinuteFutureSuffix", " ពីឥឡូវនេះ"},
+            {"MinutePastPrefix", ""},
+            {"MinutePastSuffix", " មុន"},
+            {"MinuteSingularName", "នាទី"},
+            {"MinutePluralName", "នាទី"},
+            {"MonthPattern", "%n %u"},
+            {"MonthFuturePrefix", ""},
+            {"MonthFutureSuffix", " ពីឥឡូវនេះ"},
+            {"MonthPastPrefix", ""},
+            {"MonthPastSuffix", " មុន"},
+            {"MonthSingularName", "ខែ"},
+            {"MonthPluralName", "ខែ"},
+            {"SecondPattern", "%n %u"},
+            {"SecondFuturePrefix", ""},
+            {"SecondFutureSuffix", " ពីឥឡូវនេះ"},
+            {"SecondPastPrefix", ""},
+            {"SecondPastSuffix", " មុន"},
+            {"SecondSingularName", "វិនាទី"},
+            {"SecondPluralName", "វិនាទី"},
+            {"WeekPattern", "%n %u"},
+            {"WeekFuturePrefix", ""},
+            {"WeekFutureSuffix", " ពីឥឡូវនេះ"},
+            {"WeekPastPrefix", ""},
+            {"WeekPastSuffix", " មុន"},
+            {"WeekSingularName", "សប្តាហ៍"},
+            {"WeekPluralName", "សប្តាហ៍"},
+            {"YearPattern", "%n %u"},
+            {"YearFuturePrefix", ""},
+            {"YearFutureSuffix", " ពីឥឡូវនេះ"},
+            {"YearPastPrefix", ""},
+            {"YearPastSuffix", " មុន"},
+            {"YearSingularName", "ឆ្នាំ"},
+            {"YearPluralName", "ឆ្នាំ"},
+            {"AbstractTimeUnitPattern", ""},
+            {"AbstractTimeUnitFuturePrefix", ""},
+            {"AbstractTimeUnitFutureSuffix", ""},
+            {"AbstractTimeUnitPastPrefix", ""},
+            {"AbstractTimeUnitPastSuffix", ""},
+            {"AbstractTimeUnitSingularName", ""},
+            {"AbstractTimeUnitPluralName", ""}};
+
+    @Override
+    public Object[][] getContents() {
+        return OBJECTS;
+    }
+
+}

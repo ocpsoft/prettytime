78a80f2c6ef6ee14ae598043954deba53ee75bed
ricocaribe <jmricop@gmail.com>
2018-02-28 10:23:35 +0100

    Create Resources_gl.java

---
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_gl.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+public class Resources_gl extends ListResourceBundle
+{
+
+   private static final Object[][] OBJECTS = new Object[][] {
+            { "CenturyPattern", "%n %u" },
+            { "CenturyFuturePrefix", "en " },
+            { "CenturyFutureSuffix", "" },
+            { "CenturyPastPrefix", "fai " },
+            { "CenturyPastSuffix", "" },
+            { "CenturySingularName", "século" },
+            { "CenturyPluralName", "séculos" },
+            { "DayPattern", "%n %u" },
+            { "DayFuturePrefix", "en" },
+            { "DayFutureSuffix", "" },
+            { "DayPastPrefix", "fai " },
+            { "DayPastSuffix", "" },
+            { "DaySingularName", "día " },
+            { "DayPluralName", "días" },
+            { "DecadePattern", "%acn %u" },
+            { "DecadeFuturePrefix", "en " },
+            { "DecadeFutureSuffix", "" },
+            { "DecadePastPrefix", "fai " },
+            { "DecadePastSuffix", "" },
+            { "DecadeSingularName", "década" },
+            { "DecadePluralName", "décadas" },
+            { "HourPattern", "%n %u" },
+            { "HourFuturePrefix", "en " },
+            { "HourFutureSuffix", "" },
+            { "HourPastPrefix", "fai " },
+            { "HourPastSuffix", "" },
+            { "HourSingularName", "hora" },
+            { "HourPluralName", "horas" },
+            { "JustNowPattern", "%u" },
+            { "JustNowFuturePrefix", "" },
+            { "JustNowFutureSuffix", "fai un momento" },
+            { "JustNowPastPrefix", "hai momentos" },
+            { "JustNowPastSuffix", "" },
+            { "JustNowSingularName", "" },
+            { "JustNowPluralName", "" },
+            { "MillenniumPattern", "%n %u" },
+            { "MillenniumFuturePrefix", "en " },
+            { "MillenniumFutureSuffix", "" },
+            { "MillenniumPastPrefix", "fai " },
+            { "MillenniumPastSuffix", "" },
+            { "MillenniumSingularName", "milenio" },
+            { "MillenniumPluralName", "milenios" },
+            { "MillisecondPattern", "%n %u" },
+            { "MillisecondFuturePrefix", "en " },
+            { "MillisecondFutureSuffix", "" },
+            { "MillisecondPastPrefix", "fai " },
+            { "MillisecondPastSuffix", "" },
+            { "MillisecondSingularName", "milisegundo" },
+            { "MillisecondPluralName", "milisegundos" },
+            { "MinutePattern", "%n %u" },
+            { "MinuteFuturePrefix", "en " },
+            { "MinuteFutureSuffix", "" },
+            { "MinutePastPrefix", "fai " },
+            { "MinutePastSuffix", "" },
+            { "MinuteSingularName", "minuto" },
+            { "MinutePluralName", "minutos" },
+            { "MonthPattern", "%n %u" },
+            { "MonthFuturePrefix", "en " },
+            { "MonthFutureSuffix", "" },
+            { "MonthPastPrefix", "fai " },
+            { "MonthPastSuffix", "" },
+            { "MonthSingularName", "mes" },
+            { "MonthPluralName", "meses" },
+            { "SecondPattern", "%n %u" },
+            { "SecondFuturePrefix", "en " },
+            { "SecondFutureSuffix", "" },
+            { "SecondPastPrefix", "fai " },
+            { "SecondPastSuffix", "" },
+            { "SecondSingularName", "segundo" },
+            { "SecondPluralName", "segundos" },
+            { "WeekPattern", "%n %u" },
+            { "WeekFuturePrefix", "en " },
+            { "WeekFutureSuffix", "" },
+            { "WeekPastPrefix", "fai " },
+            { "WeekPastSuffix", "" },
+            { "WeekSingularName", "semana" },
+            { "WeekPluralName", "semanas" },
+            { "YearPattern", "%n %u" },
+            { "YearFuturePrefix", "en " },
+            { "YearFutureSuffix", "" },
+            { "YearPastPrefix", "fai " },
+            { "YearPastSuffix", "" },
+            { "YearSingularName", "ano" },
+            { "YearPluralName", "anos" },
+            { "AbstractTimeUnitPattern", "" },
+            { "AbstractTimeUnitFuturePrefix", "" },
+            { "AbstractTimeUnitFutureSuffix", "" },
+            { "AbstractTimeUnitPastPrefix", "" },
+            { "AbstractTimeUnitPastSuffix", "" },
+            { "AbstractTimeUnitSingularName", "" },
+            { "AbstractTimeUnitPluralName", "" } };
+
+   @Override
+   protected Object[][] getContents()
+   {
+      return OBJECTS;
+   }
+}

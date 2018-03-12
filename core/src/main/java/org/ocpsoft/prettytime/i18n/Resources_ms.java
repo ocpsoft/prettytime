52e0f9a7f40475119b9feaecbf437a2ab70d20cc
Musasshi90 <laupong90@gmail.com>
2018-03-12 15:41:42 +0800

    Update Resources_ms.java
    update resources_ms.java

--- core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_ms.java
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_ms.java
@@ -1,1 +1,122 @@
 
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+public class Resources_zh extends ListResourceBundle
+{
+
+   private static final Object[][] OBJECTS = new Object[][] {
+            { "CenturyPattern", "%n %u" },
+            { "CenturyFuturePrefix", "" },
+            { "CenturyFutureSuffix", "Selepas" },
+            { "CenturyPastPrefix", "" },
+            { "CenturyPastSuffix", "Sebelum ini" },
+            { "CenturySingularName", "Abad" },
+            { "CenturyPluralName", "世纪" },
+            { "DayPattern", "%n %u" },
+            { "DayFuturePrefix", "" },
+            { "DayFutureSuffix", "Selepas" },
+            { "DayPastPrefix", "" },
+            { "DayPastSuffix", "Sebelum ini" },
+            { "DaySingularName", "Hari" },
+            { "DayPluralName", "Hari" },
+            { "DecadePattern", "%n%u" },
+            { "DecadeFuturePrefix", "" },
+            { "DecadeFutureSuffix", "Selepas" },
+            { "DecadePastPrefix", "" },
+            { "DecadePastSuffix", "Sebelum" },
+            { "DecadeSingularName", "0 Tahun" },
+            { "DecadePluralName", "0 Tahun" },
+            { "HourPattern", "%n %u" },
+            { "HourFuturePrefix", "" },
+            { "HourFutureSuffix", "Selepas" },
+            { "HourPastPrefix", "" },
+            { "HourPastSuffix", "Sebelum" },
+            { "HourSingularName", "Jam" },
+            { "HourPluralName", "Jam" },
+            { "JustNowPattern", "%u" },
+            { "JustNowFuturePrefix", "" },
+            { "JustNowFutureSuffix", "Hanya sekarang" },
+            { "JustNowPastPrefix", "Sebelum ini" },
+            { "JustNowPastSuffix", "" },
+            { "JustNowSingularName", "" },
+            { "JustNowPluralName", "" },
+            { "MillenniumPattern", "%n %u" },
+            { "MillenniumFuturePrefix", "" },
+            { "MillenniumFutureSuffix", "Selepas" },
+            { "MillenniumPastPrefix", "" },
+            { "MillenniumPastSuffix", "Sebelum ini" },
+            { "MillenniumSingularName", "Millennium" },
+            { "MillenniumPluralName", "Millennium" },
+            { "MillisecondPattern", "%n %u" },
+            { "MillisecondFuturePrefix", "" },
+            { "MillisecondFutureSuffix", "Selepas ini" },
+            { "MillisecondPastPrefix", "" },
+            { "MillisecondPastSuffix", "Sebelum" },
+            { "MillisecondSingularName", "Milisaat" },
+            { "MillisecondPluralName", "Milisaat" },
+            { "MinutePattern", "%n %u" },
+            { "MinuteFuturePrefix", "" },
+            { "MinuteFutureSuffix", "Selepas" },
+            { "MinutePastPrefix", "" },
+            { "MinutePastSuffix", "Sebelum ini" },
+            { "MinuteSingularName", "Minit" },
+            { "MinutePluralName", "Minit" },
+            { "MonthPattern", "%n %u" },
+            { "MonthFuturePrefix", "" },
+            { "MonthFutureSuffix", "Selepas" },
+            { "MonthPastPrefix", "" },
+            { "MonthPastSuffix", "Sebelum" },
+            { "MonthSingularName", "Bulan" },
+            { "MonthPluralName", "Bulan" },
+            { "SecondPattern", "%n %u" },
+            { "SecondFuturePrefix", "" },
+            { "SecondFutureSuffix", "Selepas" },
+            { "SecondPastPrefix", "" },
+            { "SecondPastSuffix", "Sebelum ini" },
+            { "SecondSingularName", "saat" },
+            { "SecondPluralName", "saat" },
+            { "WeekPattern", "%n %u" },
+            { "WeekFuturePrefix", "" },
+            { "WeekFutureSuffix", "Selepas" },
+            { "WeekPastPrefix", "" },
+            { "WeekPastSuffix", "Sebelum ini" },
+            { "WeekSingularName", "Minggu" },
+            { "WeekPluralName", "Minggu" },
+            { "YearPattern", "%n %u" },
+            { "YearFuturePrefix", "" },
+            { "YearFutureSuffix", "Selepas" },
+            { "YearPastPrefix", "" },
+            { "YearPastSuffix", "Sebelum" },
+            { "YearSingularName", "Tahun" },
+            { "YearPluralName", "Tahun" },
+            { "AbstractTimeUnitPattern", "" },
+            { "AbstractTimeUnitFuturePrefix", "" },
+            { "AbstractTimeUnitFutureSuffix", "" },
+            { "AbstractTimeUnitPastPrefix", "" },
+            { "AbstractTimeUnitPastSuffix", "" },
+            { "AbstractTimeUnitSingularName", "" },
+            { "AbstractTimeUnitPluralName", "" } };
+
+   @Override
+   protected Object[][] getContents()
+   {
+      return OBJECTS;
+   }
+}

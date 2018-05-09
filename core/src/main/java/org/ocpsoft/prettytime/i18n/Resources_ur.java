d0c45fabb61cdc453de272b4769dda79437b09db
ali.akram <pycppNPhY45sXHZTRDBY>
2018-05-09 16:09:44 +0500

    Added Urdu unit

--- core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_ur.java
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_ur.java
@@ -1,1 +1,123 @@
-hello
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+public class Resources_en extends ListResourceBundle
+{
+   private static final Object[][] OBJECTS = new Object[][] {
+            
+			{ "CenturyPattern", "%n %u" },
+			{ "CenturyFuturePrefix", "" },
+			{ "CenturyFutureSuffix", " ابھی سے" },
+			{ "CenturyPastPrefix", "" },
+			{ "CenturyPastSuffix", " پہلے" },
+			{ "CenturySingularName", "صدی" },
+			{ "CenturyPluralName", "صدیوں" },
+			{ "DayPattern", "%n %u" },
+			{ "DayFuturePrefix", "" },
+			{ "DayFutureSuffix", " ابھی سے" },
+			{ "DayPastPrefix", "" },
+			{ "DayPastSuffix", " پہلے" },
+			{ "DaySingularName", "دن" },
+			{ "DayPluralName", "دنوں" },
+			{ "DecadePattern", "%n %u" },
+			{ "DecadeFuturePrefix", "" },
+			{ "DecadeFutureSuffix", " ابھی سے" },
+			{ "DecadePastPrefix", "" },
+			{ "DecadePastSuffix", " پہلے" },
+			{ "DecadeSingularName", "دہائی" },
+			{ "DecadePluralName", "دہائی" },
+			{ "HourPattern", "%n %u" },
+			{ "HourFuturePrefix", "" },
+			{ "HourFutureSuffix", " ابھی سے" },
+			{ "HourPastPrefix", "" },
+			{ "HourPastSuffix", " پہلے" },
+			{ "HourSingularName", "گھنٹہ" },
+			{ "HourPluralName", "گھنٹے" },
+			{ "JustNowPattern", "%u" },
+			{ "JustNowFuturePrefix", "" },
+			{ "JustNowFutureSuffix", "اب سے لمحات" },
+			{ "JustNowPastPrefix", "لمحات پہلے" },
+			{ "JustNowPastSuffix", "" },
+			{ "JustNowSingularName", "" },
+			{ "JustNowPluralName", "" },
+			{ "MillenniumPattern", "%n %u" },
+			{ "MillenniumFuturePrefix", "" },
+			{ "MillenniumFutureSuffix", " ابھی سے" },
+			{ "MillenniumPastPrefix", "" },
+			{ "MillenniumPastSuffix", " پہلے" },
+			{ "MillenniumSingularName", "ہزار سال" },
+			{ "MillenniumPluralName", "صدیوں" },
+			{ "MillisecondPattern", "%n %u" },
+			{ "MillisecondFuturePrefix", "" },
+			{ "MillisecondFutureSuffix", " ابھی سے" },
+			{ "MillisecondPastPrefix", "" },
+			{ "MillisecondPastSuffix", " پہلے" },
+			{ "MillisecondSingularName", "ملی سیکنڈ" },
+			{ "MillisecondPluralName", "ملی سیکنڈ" },
+			{ "MinutePattern", "%n %u" },
+			{ "MinuteFuturePrefix", "" },
+			{ "MinuteFutureSuffix", " ابھی سے" },
+			{ "MinutePastPrefix", "" },
+			{ "MinutePastSuffix", " پہلے" },
+			{ "MinuteSingularName", "منٹ" },
+			{ "MinutePluralName", "منٹس " },
+			{ "MonthPattern", "%n %u" },
+			{ "MonthFuturePrefix", "" },
+			{ "MonthFutureSuffix", " ابھی سے" },
+			{ "MonthPastPrefix", "" },
+			{ "MonthPastSuffix", " پہلے" },
+			{ "MonthSingularName", "مہینہ " },
+			{ "MonthPluralName", "مہینے" },
+			{ "SecondPattern", "%n %u" },
+			{ "SecondFuturePrefix", "" },
+			{ "SecondFutureSuffix", " ابھی سے" },
+			{ "SecondPastPrefix", "" },
+			{ "SecondPastSuffix", " پہلے" },
+			{ "SecondSingularName", "سیکنڈ" },
+			{ "SecondPluralName", " سیکنڈز" },
+			{ "WeekPattern", "%n %u" },
+			{ "WeekFuturePrefix", "" },
+			{ "WeekFutureSuffix", " ابھی سے" },
+			{ "WeekPastPrefix", "" },
+			{ "WeekPastSuffix", " پہلے" },
+			{ "WeekSingularName", "ہفتہ" },
+			{ "WeekPluralName", "ہفتے" },
+			{ "YearPattern", "%n %u" },
+			{ "YearFuturePrefix", "" },
+			{ "YearFutureSuffix", " ابھی سے" },
+			{ "YearPastPrefix", "" },
+			{ "YearPastSuffix", " پہلے" },
+			{ "YearSingularName", "سال" },
+			{ "YearPluralName", "سالو" },
+			{ "AbstractTimeUnitPattern", "" },
+			{ "AbstractTimeUnitFuturePrefix", "" },
+			{ "AbstractTimeUnitFutureSuffix", "" },
+			{ "AbstractTimeUnitPastPrefix", "" },
+			{ "AbstractTimeUnitPastSuffix", "" },
+			{ "AbstractTimeUnitSingularName", "" },
+			{ "AbstractTimeUnitPluralName", "" } };
+
+
+   @Override
+   public Object[][] getContents()
+   {
+      return OBJECTS;
+   }
+
+}

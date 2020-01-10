50824d52ded618ad9927410921a45d3096fabbc3
bopol <bopol@e.email>
2020-01-10 10:52:41 +0100

    fixed bengali file written bg instead of bn

--- core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_bn.java
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_bn.java
@@ -17,7 +17,7 @@ package org.ocpsoft.prettytime.i18n;
 
 import java.util.ListResourceBundle;
 
-public class Resources_bg extends ListResourceBundle
+public class Resources_bn extends ListResourceBundle
 {
    private static final Object[][] OBJECTS = new Object[][] {
             { "CenturyPattern", "%n %u" },

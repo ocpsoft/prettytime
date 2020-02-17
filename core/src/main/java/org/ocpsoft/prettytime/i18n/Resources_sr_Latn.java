aa34e7df6daa97a057133a4dc190b1e2ddf76d42
Michael LAGUERRE <michael@teachonmars.com>
2020-02-17 11:47:19 +0100

    Added latin serbian

---
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_sr_Latn.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+public class Resources_sr_Latn extends ListResourceBundle
+{
+
+   private static final Object[][] OBJECTS = new Object[][] 
+   {
+      {"CenturyPattern", "%n %u"}, 
+      {"CenturyFuturePrefix", "za "}, 
+      {"CenturyFutureSuffix", ""}, 
+      {"CenturyPastPrefix", "pre "}, 
+      {"CenturyPastSuffix", ""}, 
+      {"CenturySingularName", "veka"}, 
+      {"CenturyPluralName", "vekova"}, 
+      {"DayPattern", "%n %u"}, 
+      {"DayFuturePrefix", "za "}, 
+      {"DayFutureSuffix", ""}, 
+      {"DayPastPrefix", "pre "}, 
+      {"DayPastSuffix", ""}, 
+      {"DaySingularName", "dan"}, 
+      {"DayPluralName", "dana"}, 
+      {"DecadePattern", "%n %u"}, 
+      {"DecadeFuturePrefix", "za "}, 
+      {"DecadeFutureSuffix", ""}, 
+      {"DecadePastPrefix", "pre "}, 
+      {"DecadePastSuffix", ""}, 
+      {"DecadeSingularName", "deceniju"}, 
+      {"DecadePluralName", "decenija"}, 
+      {"HourPattern", "%n %u"}, 
+      {"HourFuturePrefix", "za "}, 
+      {"HourFutureSuffix", ""}, 
+      {"HourPastPrefix", "pre "}, 
+      {"HourPastSuffix", ""}, 
+      {"HourSingularName", "sat"}, 
+      {"HourPluralName", "sati"}, 
+      {"JustNowPattern", "%u"}, 
+      {"JustNowFuturePrefix", ""}, 
+      {"JustNowFutureSuffix", "upravo sada"}, 
+      {"JustNowPastPrefix", "upravo sada"}, 
+      {"JustNowPastSuffix", ""}, 
+      {"JustNowSingularName", ""}, 
+      {"JustNowPluralName", ""}, 
+      {"MillenniumPattern", "%n %u"}, 
+      {"MillenniumFuturePrefix", "za "}, 
+      {"MillenniumFutureSuffix", ""}, 
+      {"MillenniumPastPrefix", "pre "}, 
+      {"MillenniumPastSuffix", ""}, 
+      {"MillenniumSingularName", "milenijuma"}, 
+      {"MillenniumPluralName", "milenijuma"}, 
+      {"MillisecondPattern", "%n %u"}, 
+      {"MillisecondFuturePrefix", "za "}, 
+      {"MillisecondFutureSuffix", ""}, 
+      {"MillisecondPastPrefix", "pre "}, 
+      {"MillisecondPastSuffix", ""}, 
+      {"MillisecondSingularName", "milisekunde"}, 
+      {"MillisecondPluralName", "milisekundi"}, 
+      {"MinutePattern", "%n %u"}, 
+      {"MinuteFuturePrefix", "za "}, 
+      {"MinuteFutureSuffix", ""}, 
+      {"MinutePastPrefix", "pre "}, 
+      {"MinutePastSuffix", ""}, 
+      {"MinuteSingularName", "minut"}, 
+      {"MinutePluralName", "minuta"}, 
+      {"MonthPattern", "%n %u"}, 
+      {"MonthFuturePrefix", "za "}, 
+      {"MonthFutureSuffix", ""}, 
+      {"MonthPastPrefix", "pre "}, 
+      {"MonthPastSuffix", ""}, 
+      {"MonthSingularName", "mesec"}, 
+      {"MonthPluralName", "meseca"}, 
+      {"SecondPattern", "%n %u"}, 
+      {"SecondFuturePrefix", "za "}, 
+      {"SecondFutureSuffix", ""}, 
+      {"SecondPastPrefix", "pre "}, 
+      {"SecondPastSuffix", ""}, 
+      {"SecondSingularName", "sekundu"}, 
+      {"SecondPluralName", "sekunde"}, 
+      {"WeekPattern", "%n %u"}, 
+      {"WeekFuturePrefix", "za "}, 
+      {"WeekFutureSuffix", ""}, 
+      {"WeekPastPrefix", "pre "}, 
+      {"WeekPastSuffix", ""}, 
+      {"WeekSingularName", "nedelju"}, 
+      {"WeekPluralName", "nedelje"}, 
+      {"YearPattern", "%n %u"}, 
+      {"YearFuturePrefix", "za "}, 
+      {"YearFutureSuffix", ""}, 
+      {"YearPastPrefix", "pre "}, 
+      {"YearPastSuffix", ""}, 
+      {"YearSingularName", "godinu"}, 
+      {"YearPluralName", "godine"}, 
+      {"AbstractTimeUnitPattern", ""}, 
+      {"AbstractTimeUnitFuturePrefix", ""}, 
+      {"AbstractTimeUnitFutureSuffix", ""}, 
+      {"AbstractTimeUnitPastPrefix", ""}, 
+      {"AbstractTimeUnitPastSuffix", ""}, 
+      {"AbstractTimeUnitSingularName", ""}, 
+      {"AbstractTimeUnitPluralName", ""} };
+
+   @Override
+   protected Object[][] getContents()
+   {
+      return OBJECTS;
+   }
+}

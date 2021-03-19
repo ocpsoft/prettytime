8f374c56d6edfc4eaa0eb5647f25acac444db674
nadiration <59365236+nadiration@users.noreply.github.com>
2021-03-19 16:41:59 +0300

    Added Somali language

---
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_so.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+public class Resources_en extends ListResourceBundle
+{
+   private static final Object[][] OBJECTS = new Object[][] {
+            { "CenturyPattern", "%n %u" },
+            { "CenturyFuturePrefix", "" },
+            { "CenturyFutureSuffix", " kadib" },
+            { "CenturyPastPrefix", "" },
+            { "CenturyPastSuffix", " kahor" },
+            { "CenturySingularName", "qarni" },
+            { "CenturyPluralName", "qarni" },
+            { "DayPattern", "%n %u" },
+            { "DayFuturePrefix", "" },
+            { "DayFutureSuffix", " kadib" },
+            { "DayPastPrefix", "" },
+            { "DayPastSuffix", " kahor" },
+            { "DaySingularName", "maalin" },
+            { "DayPluralName", "maalmood" },
+            { "DecadePattern", "%n %u" },
+            { "DecadeFuturePrefix", "" },
+            { "DecadeFutureSuffix", " kadib" },
+            { "DecadePastPrefix", "" },
+            { "DecadePastSuffix", " kahor" },
+            { "DecadeSingularName", "toban sano" },
+            { "DecadePluralName", "toban sanooyin" },
+            { "HourPattern", "%n %u" },
+            { "HourFuturePrefix", "" },
+            { "HourFutureSuffix", " kadib" },
+            { "HourPastPrefix", "" },
+            { "HourPastSuffix", " kahor" },
+            { "HourSingularName", "saac" },
+            { "HourPluralName", "saac" },
+            { "JustNowPattern", "%u" },
+            { "JustNowFuturePrefix", "" },
+            { "JustNowFutureSuffix", "waxyar kadib" },
+            { "JustNowPastPrefix", "waxyar kahor" },
+            { "JustNowPastSuffix", "" },
+            { "JustNowSingularName", "" },
+            { "JustNowPluralName", "" },
+            { "MillenniumPattern", "%n %u" },
+            { "MillenniumFuturePrefix", "" },
+            { "MillenniumFutureSuffix", " kadib" },
+            { "MillenniumPastPrefix", "" },
+            { "MillenniumPastSuffix", " kahor" },
+            { "MillenniumSingularName", "kun sano" },
+            { "MillenniumPluralName", "kun sano" },
+            { "MillisecondPattern", "%n %u" },
+            { "MillisecondFuturePrefix", "" },
+            { "MillisecondFutureSuffix", " kadib" },
+            { "MillisecondPastPrefix", "" },
+            { "MillisecondPastSuffix", " ago" },
+            { "MillisecondSingularName", "ilbiriqsi kayar" },
+            { "MillisecondPluralName", "ilbiriqsi kayar" },
+            { "MinutePattern", "%n %u" },
+            { "MinuteFuturePrefix", "" },
+            { "MinuteFutureSuffix", " kadib" },
+            { "MinutePastPrefix", "" },
+            { "MinutePastSuffix", " kahor" },
+            { "MinuteSingularName", "daqiiqo" },
+            { "MinutePluralName", "daqiiqo" },
+            { "MonthPattern", "%n %u" },
+            { "MonthFuturePrefix", "" },
+            { "MonthFutureSuffix", " kadib" },
+            { "MonthPastPrefix", "" },
+            { "MonthPastSuffix", " kahor" },
+            { "MonthSingularName", "bil" },
+            { "MonthPluralName", "bilood" },
+            { "SecondPattern", "%n %u" },
+            { "SecondFuturePrefix", "" },
+            { "SecondFutureSuffix", " kadib" },
+            { "SecondPastPrefix", "" },
+            { "SecondPastSuffix", " kahor" },
+            { "SecondSingularName", "ilbiriqsi" },
+            { "SecondPluralName", "ilbiriqsi" },
+            { "WeekPattern", "%n %u" },
+            { "WeekFuturePrefix", "" },
+            { "WeekFutureSuffix", " kadib" },
+            { "WeekPastPrefix", "" },
+            { "WeekPastSuffix", " kahor" },
+            { "WeekSingularName", "todobaad" },
+            { "WeekPluralName", "todobaad" },
+            { "YearPattern", "%n %u" },
+            { "YearFuturePrefix", "" },
+            { "YearFutureSuffix", " kadib" },
+            { "YearPastPrefix", "" },
+            { "YearPastSuffix", " kahor" },
+            { "YearSingularName", "sano" },
+            { "YearPluralName", "sano" },
+            { "AbstractTimeUnitPattern", "" },
+            { "AbstractTimeUnitFuturePrefix", "" },
+            { "AbstractTimeUnitFutureSuffix", "" },
+            { "AbstractTimeUnitPastPrefix", "" },
+            { "AbstractTimeUnitPastSuffix", "" },
+            { "AbstractTimeUnitSingularName", "" },
+            { "AbstractTimeUnitPluralName", "" } };
+
+   @Override
+   public Object[][] getContents()
+   {
+      return OBJECTS;
+   }
+
+}

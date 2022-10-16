c12b32c02104619f08374a2898141eda97377fd9
Maqa <104249296+Nizami20052022@users.noreply.github.com>
2022-10-16 18:08:31 +0400

    Added resources_az.java.
    Added Azerbaijani language.

---
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_az.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+public class Resources_az extends ListResourceBundle
+{
+   private static final Object[][] OBJECTS = new Object[][] {
+            { "CenturyPattern", "%n %u" },
+            { "CenturyFuturePrefix", "" },
+            { "CenturyFutureSuffix", " indidən" },
+            { "CenturyPastPrefix", "" },
+            { "CenturyPastSuffix", " əvvəl" },
+            { "CenturySingularName", "əsr" },
+            { "CenturyPluralName", "əsr" },
+            { "DayPattern", "%n %u" },
+            { "DayFuturePrefix", "" },
+            { "DayFutureSuffix", " indidən" },
+            { "DayPastPrefix", "" },
+            { "DayPastSuffix", " əvvəl" },
+            { "DaySingularName", "gün" },
+            { "DayPluralName", "gün" },
+            { "DecadePattern", "%n %u" },
+            { "DecadeFuturePrefix", "" },
+            { "DecadeFutureSuffix", " indidən" },
+            { "DecadePastPrefix", "" },
+            { "DecadePastSuffix", " əvvəl" },
+            { "DecadeSingularName", "onillik" },
+            { "DecadePluralName", "onillik" },
+            { "HourPattern", "%n %u" },
+            { "HourFuturePrefix", "" },
+            { "HourFutureSuffix", " indidən" },
+            { "HourPastPrefix", "" },
+            { "HourPastSuffix", " əvvəl" },
+            { "HourSingularName", "saat" },
+            { "HourPluralName", "saat" },
+            { "JustNowPattern", "%u" },
+            { "JustNowFuturePrefix", "" },
+            { "JustNowFutureSuffix", "bundan sonra yerdəyişmə" },
+            { "JustNowPastPrefix", "dəqiqə əvvəl" },
+            { "JustNowPastSuffix", "" },
+            { "JustNowSingularName", "" },
+            { "JustNowPluralName", "" },
+            { "MillenniumPattern", "%n %u" },
+            { "MillenniumFuturePrefix", "" },
+            { "MillenniumFutureSuffix", "indidən" },
+            { "MillenniumPastPrefix", "" },
+            { "MillenniumPastSuffix", " əvvəl" },
+            { "MillenniumSingularName", "minillik" },
+            { "MillenniumPluralName", "minillik" },
+            { "MillisecondPattern", "%n %u" },
+            { "MillisecondFuturePrefix", "" },
+            { "MillisecondFutureSuffix", " indidən" },
+            { "MillisecondPastPrefix", "" },
+            { "MillisecondPastSuffix", " əvvəl" },
+            { "MillisecondSingularName", "millisaniyə" },
+            { "MillisecondPluralName", "millisaniyə" },
+            { "MinutePattern", "%n %u" },
+            { "MinuteFuturePrefix", "" },
+            { "MinuteFutureSuffix", " indidən" },
+            { "MinutePastPrefix", "" },
+            { "MinutePastSuffix", " əvvəl" },
+            { "MinuteSingularName", "dəqiqə" },
+            { "MinutePluralName", "dəqiqə" },
+            { "MonthPattern", "%n %u" },
+            { "MonthFuturePrefix", "" },
+            { "MonthFutureSuffix", " indidən" },
+            { "MonthPastPrefix", "" },
+            { "MonthPastSuffix", " əvvəl" },
+            { "MonthSingularName", "ay" },
+            { "MonthPluralName", "ay" },
+            { "SecondPattern", "%n %u" },
+            { "SecondFuturePrefix", "" },
+            { "SecondFutureSuffix", " indidən" },
+            { "SecondPastPrefix", "" },
+            { "SecondPastSuffix", " əvvəl" },
+            { "SecondSingularName", "saniyə" },
+            { "SecondPluralName", "saniyə" },
+            { "WeekPattern", "%n %u" },
+            { "WeekFuturePrefix", "" },
+            { "WeekFutureSuffix", " indidən" },
+            { "WeekPastPrefix", "" },
+            { "WeekPastSuffix", " əvvəl" },
+            { "WeekSingularName", "həftə" },
+            { "WeekPluralName", "həftə" },
+            { "YearPattern", "%n %u" },
+            { "YearFuturePrefix", "" },
+            { "YearFutureSuffix", " indidən" },
+            { "YearPastPrefix", "" },
+            { "YearPastSuffix", " əvvəl" },
+            { "YearSingularName", "il" },
+            { "YearPluralName", "il" },
+            { "AbstractTimeUnitPattern", "" },
+            { "AbstractTimeUnitFuturePrefix", "" },
+            { "AbstractTimeUnitFutureSuffix", "" },
+            { "AbstractTimeUnitPastPrefix", "" },
+            { "AbstractTimeUnitPastSuffix", "" },
+            { "AbstractTimeUnitSingularName", "" },
+            { "AbstractTimeUnitPluralName", "" } };
+
+   @Override
+   public Object[][] getContents()
+   {
+      return OBJECTS;
+   }
+
+}

6ab06f52e82cb5c14f2cd521192134080e76473d
ShareASmile <60492161+ShareASmile@users.noreply.github.com>
2022-10-17 07:06:17 +0530

    translation in punjabi language
    Added Translation in Punjabi language

---
+++ core/src/main/java/org/ocpsoft/prettytime/i18n/Resources_pa.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2012 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.prettytime.i18n;
+
+import java.util.ListResourceBundle;
+
+public class Resources_pa extends ListResourceBundle
+{
+
+   private static final Object[][] OBJECTS = new Object[][] {
+            { "CenturyPattern", "%n %u" },
+            { "CenturyFuturePrefix", "" },
+            { "CenturyFutureSuffix", " ਬਾਅਦ" },
+            { "CenturyPastPrefix", "" },
+            { "CenturyPastSuffix", " ਪਹਿਲਾਂ" },
+            { "CenturySingularName", "ਸਦੀ" },
+            { "CenturyPluralName", "ਸਦੀਆਂ" },
+            { "DayPattern", "%n %u" },
+            { "DayFuturePrefix", "" },
+            { "DayFutureSuffix", " ਬਾਅਦ" },
+            { "DayPastPrefix", "" },
+            { "DayPastSuffix", " ਪਹਿਲਾਂ" },
+            { "DaySingularName", "ਦਿਨ" },
+            { "DayPluralName", "ਦਿਨ" },
+            { "DecadePattern", "%n %u" },
+            { "DecadeFuturePrefix", "" },
+            { "DecadeFutureSuffix", " ਬਾਅਦ" },
+            { "DecadePastPrefix", "" },
+            { "DecadePastSuffix", " ਪਹਿਲਾਂ" },
+            { "DecadeSingularName", "ਦਹਾਕਾ" },
+            { "DecadePluralName", "ਦਹਾਕੇ" },
+            { "HourPattern", "%n %u" },
+            { "HourFuturePrefix", "" },
+            { "HourFutureSuffix", " ਬਾਅਦ" },
+            { "HourPastPrefix", "" },
+            { "HourPastSuffix", " ਪਹਿਲਾਂ" },
+            { "HourSingularName", "ਘੰਟਾ" },
+            { "HourPluralName", "ਘੰਟੇ" },
+            { "JustNowPattern", "%u" },
+            { "JustNowFuturePrefix", "" },
+            { "JustNowFutureSuffix", "ਕੁਝ ਪਲ ਬਾਅਦ" },
+            { "JustNowPastPrefix", "ਹੁਣੇ-ਹੁਣੇ" },
+            { "JustNowPastSuffix", "" },
+            { "JustNowSingularName", "" },
+            { "JustNowPluralName", "" },
+            { "MillenniumPattern", "%n %u" },
+            { "MillenniumFuturePrefix", "" },
+            { "MillenniumFutureSuffix", " ਬਾਅਦ" },
+            { "MillenniumPastPrefix", "" },
+            { "MillenniumPastSuffix", " ਪਹਿਲਾਂ" },
+            { "MillenniumSingularName", "ਸ਼ਤਾਬਦੀ" },
+            { "MillenniumPluralName", "ਸ਼ਤਾਬਦੀਆਂ" },
+            { "MillisecondPattern", "%n %u" },
+            { "MillisecondFuturePrefix", "" },
+            { "MillisecondFutureSuffix", " ਬਾਅਦ" },
+            { "MillisecondPastPrefix", "" },
+            { "MillisecondPastSuffix", " ਪਹਿਲਾਂ" },
+            { "MillisecondSingularName", "ਮਿਲੀਸਕਿੰਟ" },
+            { "MillisecondPluralName", "ਮਿਲੀਸਕਿੰਟ" },
+            { "MinutePattern", "%n %u" },
+            { "MinuteFuturePrefix", "" },
+            { "MinuteFutureSuffix", " ਬਾਅਦ" },
+            { "MinutePastPrefix", "" },
+            { "MinutePastSuffix", " ਪਹਿਲਾਂ" },
+            { "MinuteSingularName", "ਮਿੰਟ" },
+            { "MinutePluralName", "ਮਿੰਟ" },
+            { "MonthPattern", "%n %u" },
+            { "MonthFuturePrefix", "" },
+            { "MonthFutureSuffix", " ਬਾਅਦ" },
+            { "MonthPastPrefix", "" },
+            { "MonthPastSuffix", " ਪਹਿਲਾਂ" },
+            { "MonthSingularName", "ਮਹੀਨਾ" },
+            { "MonthPluralName", "ਮਹੀਨੇ" },
+            { "SecondPattern", "%n %u" },
+            { "SecondFuturePrefix", "" },
+            { "SecondFutureSuffix", " ਬਾਅਦ" },
+            { "SecondPastPrefix", "" },
+            { "SecondPastSuffix", " ਪਹਿਲਾਂ" },
+            { "SecondSingularName", "ਸਕਿੰਟ" },
+            { "SecondPluralName", "ਸਕਿੰਟ" },
+            { "WeekPattern", "%n %u" },
+            { "WeekFuturePrefix", "" },
+            { "WeekFutureSuffix", " ਬਾਅਦ" },
+            { "WeekPastPrefix", "" },
+            { "WeekPastSuffix", " ਪਹਿਲਾਂ" },
+            { "WeekSingularName", "ਹਫ਼ਤਾ" },
+            { "WeekPluralName", "ਹਫ਼ਤੇ" },
+            { "YearPattern", "%n %u" },
+            { "YearFuturePrefix", "" },
+            { "YearFutureSuffix", " ਬਾਅਦ" },
+            { "YearPastPrefix", "" },
+            { "YearPastSuffix", " ਪਹਿਲਾਂ" },
+            { "YearSingularName", "ਸਾਲ" },
+            { "YearPluralName", "ਸਾਲ" },
+            { "AbstractTimeUnitPattern", "" },
+            { "AbstractTimeUnitFuturePrefix", "" },
+            { "AbstractTimeUnitFutureSuffix", "" },
+            { "AbstractTimeUnitPastPrefix", "" },
+            { "AbstractTimeUnitPastSuffix", "" },
+            { "AbstractTimeUnitSingularName", "" },
+            { "AbstractTimeUnitPluralName", "" } };
+
+   @Override
+   public Object[][] getContents()
+   {
+      return OBJECTS;
+   }
+
+}

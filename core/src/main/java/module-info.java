9c62f6fa4e5f54b9025ff98834851d488bed8a46
webracer999 <992620+webracer999@users.noreply.github.com>
2023-08-07 20:12:48 +0200

    Create MRJAR with module-info.class

---
+++ core/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module prettytime {
+    exports org.ocpsoft.prettytime;
+    exports org.ocpsoft.prettytime.format;
+    exports org.ocpsoft.prettytime.i18n;
+    exports org.ocpsoft.prettytime.impl;
+    exports org.ocpsoft.prettytime.units;
+    provides org.ocpsoft.prettytime.PrettyTime with org.ocpsoft.prettytime.PrettyTime;
+}

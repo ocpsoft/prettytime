3cb9a93ef2408844c27ccf6146af7d0ab8ff091a
Spiros Vita <vitabytes@proton.me>
2025-07-19 16:25:36 +0300

    Fix bad Greek translations and update the tests accordingly

--- core/src/test/java/org/ocpsoft/prettytime/PrettyTimeI18n_EL_Test.java
+++ core/src/test/java/org/ocpsoft/prettytime/PrettyTimeI18n_EL_Test.java
@@ -31,7 +31,7 @@ public class PrettyTimeI18n_EL_Test
         Date then = format.parse("5/20/2009");
         Date ref = format.parse("6/17/2009");
         PrettyTime t = new PrettyTime(ref);
-        Assert.assertEquals("1 μήνας Πριν από", t.format(then));
+        Assert.assertEquals("Πριν από 1 μήνα", t.format(then));
     }
 
     @Test
@@ -56,7 +56,7 @@ public class PrettyTimeI18n_EL_Test
         PrettyTime t = new PrettyTime(now);
         List<Duration> preciseDuration = t.calculatePreciseDuration(
                     new Date(System.currentTimeMillis() - (2 * 60 * 60 * 1000) - (2 * 60 * 1000)));
-        Assert.assertEquals("2 ώρες 2 λεπτά Πριν από", t.format(preciseDuration));
+        Assert.assertEquals("Πριν από 2 ώρες 2 λεπτά", t.format(preciseDuration));
         Assert.assertEquals("2 ώρες 2 λεπτά", t.formatDuration(preciseDuration));
         Assert.assertEquals("στιγμές από τώρα", t.format(t.calculatePreciseDuration(now)));
     }
@@ -68,7 +68,7 @@ public class PrettyTimeI18n_EL_Test
         prettyTime.clearUnits();
         Minute minutes = new Minute();
         prettyTime.registerUnit(minutes, new ResourcesTimeFormat(minutes));
-        Assert.assertEquals("40 λεπτά Πριν από", prettyTime.formatUnrounded(prettyTime.calculatePreciseDuration(
+        Assert.assertEquals("Πριν από 40 λεπτά", prettyTime.formatUnrounded(prettyTime.calculatePreciseDuration(
                     new Date(new Date().getTime() - 40 * 60 * 1000 - 40 * 1000))));
     }
 
@@ -149,7 +149,7 @@ public class PrettyTimeI18n_EL_Test
     public void testMinutesAgo() throws Exception
     {
         PrettyTime t = new PrettyTime(new Date(1000 * 60 * 12));
-        Assert.assertEquals("12 λεπτά Πριν από", t.format(new Date(0)));
+        Assert.assertEquals("Πριν από 12 λεπτά", t.format(new Date(0)));
     }
 
     @Test
@@ -163,63 +163,63 @@ public class PrettyTimeI18n_EL_Test
     public void testHoursAgo() throws Exception
     {
         PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 3));
-        Assert.assertEquals("3 ώρες Πριν από", t.format(new Date(0)));
+        Assert.assertEquals("Πριν από 3 ώρες", t.format(new Date(0)));
     }
 
     @Test
     public void testHoursAgoDefaultReference() throws Exception
     {
         PrettyTime t = new PrettyTime();
-        Assert.assertEquals("3 ώρες Πριν από", t.format(new Date(System.currentTimeMillis() - 1000 * 60 * 60 * 3)));
+        Assert.assertEquals("Πριν από 3 ώρες", t.format(new Date(System.currentTimeMillis() - 1000 * 60 * 60 * 3)));
     }
 
     @Test
     public void testDaysAgo() throws Exception
     {
         PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 24 * 3));
-        Assert.assertEquals("3 ημέρες Πριν από", t.format(new Date(0)));
+        Assert.assertEquals("Πριν από 3 ημέρες", t.format(new Date(0)));
     }
 
     @Test
     public void testWeeksAgo() throws Exception
     {
         PrettyTime t = new PrettyTime(new Date(1000 * 60 * 60 * 24 * 7 * 3));
-        Assert.assertEquals("3 εβδομάδες Πριν από", t.format(new Date(0)));
+        Assert.assertEquals("Πριν από 3 εβδομάδες", t.format(new Date(0)));
     }
 
     @Test
     public void testMonthsAgo() throws Exception
     {
         PrettyTime t = new PrettyTime(new Date(2629743830L * 3L));
-        Assert.assertEquals("3 μήνες Πριν από", t.format(new Date(0)));
+        Assert.assertEquals("Πριν από 3 μήνες", t.format(new Date(0)));
     }
 
     @Test
     public void testYearsAgo() throws Exception
     {
         PrettyTime t = new PrettyTime(new Date(2629743830L * 12L * 3L));
-        Assert.assertEquals("3 έτη Πριν από", t.format(new Date(0)));
+        Assert.assertEquals("Πριν από 3 έτη", t.format(new Date(0)));
     }
 
     @Test
     public void testDecadesAgo() throws Exception
     {
         PrettyTime t = new PrettyTime(new Date(315569259747L * 3L));
-        Assert.assertEquals("3 δεκαετίες Πριν από", t.format(new Date(0)));
+        Assert.assertEquals("Πριν από 3 δεκαετίες", t.format(new Date(0)));
     }
 
     @Test
     public void testCenturiesAgo() throws Exception
     {
         PrettyTime t = new PrettyTime(new Date(3155692597470L * 3L));
-        Assert.assertEquals("3 αιώνες Πριν από", t.format(new Date(0)));
+        Assert.assertEquals("Πριν από 3 αιώνες", t.format(new Date(0)));
     }
 
     @Test
     public void testWithinTwoHoursRounding() throws Exception
     {
         PrettyTime t = new PrettyTime();
-        Assert.assertEquals("2 ώρες Πριν από", t.format(new Date(new Date().getTime() - 6543990)));
+        Assert.assertEquals("Πριν από 2 ώρες", t.format(new Date(new Date().getTime() - 6543990)));
     }
 
     @After
